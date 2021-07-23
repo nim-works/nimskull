1faa0f3a5e711c721e74ea2e38e4210789efc8fb
Saem Ghani <saemghani+github@gmail.com>
2021-07-22 19:01:56 -0700

    tests for arrays and sequences
    - add basica array and sequence tests
    - fixed pre-existing array tests to cover c, cpp, and js platforms

---
+++ tests/lang/s01_basics/s07_arrays_sequences/t02_sequence.nim
@@ -0,0 +1,42 @@
+discard """
+description: '''
+Describe the basics of sequences:
+- dynamically sized list of a single type
+- literals
+- indexing
+- length
+'''
+target: "c cpp js"
+"""
+
+block literal:
+  let
+    a: seq[int] = @[]
+    b = @[1, 2, 3]
+  doAssert a == @[],           "two empty seqs are equal"
+  doAssert b == @[1, 2, 3],    "seq elements are compared by element pair"
+  doAssert b != @[1, 2, 3, 4], "seq's don't partially match"
+
+block seq_len:
+  var
+    a: seq[int]
+    b = @[1, 2, 3, 4, 5, 6, 7, 8 , 9, 10]
+  doAssert a.len == 0,  "an empty seq has len 0"
+  doAssert b.len == 10, "seq len is the count of elements"
+
+block seq_indexing:
+  var a = @[1, 2]
+
+  doAssert a[0] == 1, "seqs by default are zero based indexed"
+  a[0] = 4
+  doAssert a[0] == 4, "seqs update in place"
+
+block seq_different_types:
+  # seqs can have more than just the `int` type
+  var
+    a = @["foo", "bar", "bar"]
+    b = @[@[1], @[2, 3], @[4, 5, 6]]
+  doAssert a[2] == "bar"
+  a[2] = "baz"
+  doAssert a[2] == "baz", "element was updated"
+  doAssert b[1][1] == 3,  "chain index look ups work"
