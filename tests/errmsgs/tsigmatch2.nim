92c5ed7e3a326d87d15927d088ca19bb627e6940
haxscramper <haxscramper@gmail.com>
2022-01-14 23:34:22 +0300

    [CHECKPOINT]

--- tests/errmsgs/tsigmatch2.nim
+++ tests/errmsgs/tsigmatch2.nim
@@ -1,5 +1,6 @@
 discard """
   cmd: "nim check --showAllMismatches:on --hints:off $file"
+  action: reject
   nimout: '''
 tsigmatch2.nim(40, 14) Error: type mismatch: got <float64>
 but expected one of:
@@ -13,7 +14,7 @@ proc foo(i: Foo): string
   but expression '1.2' is of type: float64
 
 expression: foo(1.2)
-tsigmatch2.nim(40, 14) Error: expression '' has no type (or is ambiguous)
+tsigmatch2.nim(40, 14) Error: expression has no type: foo(1.2)
 tsigmatch2.nim(46, 7) Error: type mismatch: got <int literal(1)>
 but expected one of:
 proc foo(args: varargs[string, myproc])
@@ -23,7 +24,6 @@ proc foo(args: varargs[string, myproc])
 
 expression: foo 1
 '''
-  errormsg: "type mismatch"
 """
 
 
