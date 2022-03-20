8aeb8f1a93e420a518a68eaabfcd51ebad8d04dd
Saem Ghani <saemghani+github@gmail.com>
2022-03-20 15:54:34 -0700

    lang spec: don't test union pragma in js

--- tests/lang/s05_pragmas/s02_misc/t06_object.nim
+++ tests/lang/s05_pragmas/s02_misc/t06_object.nim
@@ -1,24 +1,25 @@
 block union_pragma:
-  ## The union pragma can be applied to any object type. It means all of the object's 
-  ## fields are overlaid in memory.This produces a union instead of a struct in the 
-  ## generated C/C++ code.
-  type
-    Union {.union.} = object
-      field1: int
-      field2: array[12, int]
-      field3: tuple[idx1, idx2: int]
+  when not defined(js): # union doesn't work yet (could use `DataFrame`s) in JS
+    ## The union pragma can be applied to any object type. It means all of the
+    ## object's fields are overlaid in memory. This produces a union instead of
+    ## a struct in the generated C/C++ code.
+    type
+      Union {.union.} = object
+        field1: int
+        field2: array[12, int]
+        field3: tuple[idx1, idx2: int]
 
-  doAssert sizeof(Union) == max(sizeof(int), sizeof(array[12, int]))
+    doAssert sizeof(Union) == max(sizeof(int), sizeof(array[12, int]))
 
-  var uni = Union()
+    var uni = Union()
 
-  uni.field1 = 10
+    uni.field1 = 10
 
-  doAssert uni.field2[0] == 10
+    doAssert uni.field2[0] == 10
 
-  doAssert uni.field3.idx1 == 10
+    doAssert uni.field3.idx1 == 10
 
-  uni = Union(field2: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+    uni = Union(field2: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
 
-  doAssert uni.field1 == 1
-  doAssert uni.field3 == (1, 2)
+    doAssert uni.field1 == 1
+    doAssert uni.field3 == (1, 2)
