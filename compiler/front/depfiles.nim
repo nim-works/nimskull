10f8fd4f97e50728fa895880eb70582336b1a854
Anna “CyberTailor” <cyber@sysrq.in>
2022-07-02 05:39:31 +0500

    compiler/front: add depfile option
    Add a new command-line option (--depfile) for the nim compile command.
    It accepts an argument (output path for a depfile, generated for a given
    target).
    
    This change improves integrations with language-independent build
    systems. Particularly, helps to get better edit-compile cycle times.
    
    Previously a build system needed to rebuild a binary if any of source
    files had been changed, even if they weren't used by the binary. The
    full list of files that a given source file depends on can only be
    discovered by the compiler.
    
    It doesn't need to be a standalone command because if the file has never
    been compiled, it must be built anyway, generating dependencies as a
    side effect.
    
    See PR #376 for more information and examples.

---
+++ compiler/front/depfiles.nim
@@ -0,0 +1,37 @@
+#
+#    See the file "copying.txt", included in this
+#    distribution, for details about the copyright.
+#
+
+## Functions for writing target's dependencies in various formats.
+
+import
+  std/[strutils, tables]
+
+import
+  compiler/utils/[pathutils],
+  compiler/modules/[modulegraphs],
+  compiler/ast/[ast_types],
+  compiler/front/[msgs, options]
+
+proc writeDepsFile*(g: ModuleGraph) =
+  let fname = g.config.nimcacheDir / RelativeFile(g.config.projectName & ".deps")
+  let f = open(fname.string, fmWrite)
+  for m in g.ifaces:
+    if m.module != nil:
+      f.writeLine(toFullPath(g.config, m.module.position.FileIndex))
+  for k in g.inclToMod.keys:
+    if g.getModule(k).isNil:  # don't repeat includes which are also modules
+      f.writeLine(toFullPath(g.config, k))
+  f.close()
+
+proc writeGccDepfile*(depfile: File, target: string, paths: openArray[string]) =
+  ## Outputs one `make` rule containing target's file name, a colon, and the
+  ## names of all specified paths. Spaces and hashes are escaped.
+  let target = target.multiReplace((" ", "\\ "), ("#", "\\#"))
+  depfile.write(target & ": \\" & '\n')
+  for it in paths:
+    let path = it.multiReplace((" ", "\\ "), ("#", "\\#"))
+    if path.len == 0:
+      continue
+    depfile.write('\t' & path & " \\" & '\n')
