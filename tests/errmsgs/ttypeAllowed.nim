0a4dbe70eb1022f6e388878fa8dbf822a7ceccb7
saem <saemghani+github@gmail.com>
2022-07-26 00:17:50 -0700

    macro pragmas: support template and const defs
    * constdef annotation handling same as identdefs in let or var
    * annotation processing on template definitions like other routine kinds
    
    Details:
    * reenabled `tests/pragmas/tpragmas_misc`
    * updated manual for macros pragmas
    * `semstmts.semConst` reworked to be like `semstmts.semLetOrVar`
    * constdef annotation handling same as identdefs in let or var
    * `semLetOrVarAnnotation` works on const defs, and renamed
    * added tracing to `semTemplateDef`
    * added tests for let/var section processing
    
    Mostly a port of https://github.com/nim-lang/Nim/pull/19406

--- tests/errmsgs/ttypeAllowed.nim
+++ tests/errmsgs/ttypeAllowed.nim
@@ -2,8 +2,8 @@ discard """
 cmd: "nim check $file"
 errormsg: ""
 nimout: '''
-ttypeAllowed.nim(17, 7) Error: invalid type: 'iterator (a: int, b: int, step: Positive): int{.inline, noSideEffect, gcsafe, locks: 0.}' for const
 ttypeAllowed.nim(13, 10) Error: invalid type: 'iterator (a: int, b: int, step: Positive): int{.inline, noSideEffect, gcsafe, locks: 0.}' for let
+ttypeAllowed.nim(17, 12) Error: invalid type: 'iterator (a: int, b: int, step: Positive): int{.inline, noSideEffect, gcsafe, locks: 0.}' for const
 ttypeAllowed.nim(21, 10) Error: invalid type: 'iterator (a: int, b: int, step: Positive): int{.inline, noSideEffect, gcsafe, locks: 0.}' for var
 ttypeAllowed.nim(26, 10) Error: invalid type: 'iterator (a: int, b: int, step: Positive): int{.inline, noSideEffect, gcsafe, locks: 0.}' for result
 '''
--- tests/pragmas/tpragmas_misc.nim
+++ tests/pragmas/tpragmas_misc.nim
@@ -1,68 +1,86 @@
 discard """
-    disabled: true
+description: "test macros pragmas on templates"
 """
 
 ##[
 tests for misc pragmas that don't need a separate file
 ]##
 
-block:
+block can_undefine_non_existent_define:
+  # this should work as it's idempotent
   static: doAssert not defined(tpragmas_misc_def)
   {.undef(tpragmas_misc_def).} # works even if not set
+
+block can_define_a_fresh_define:
   static: doAssert not defined(tpragmas_misc_def)
   {.define(tpragmas_misc_def).}
+
+block undef_removes_an_existing_define:
   static: doAssert defined(tpragmas_misc_def)
   {.undef(tpragmas_misc_def).}
   static: doAssert not defined(tpragmas_misc_def)
 
-block: # (partial fix) bug #15920
+block: # (partial fix) bug https://github.com/nim-lang/nim/issues/15920
   block: # var template pragmas don't work in templates
-    template foo(lhs, typ, expr) =
-      let lhs = expr
+    template foo(expr) =
+      expr
+    
     proc fun1()=
       let a {.foo.} = 1
+    
     template fun2()=
       let a {.foo.} = 1
+    
     fun1() # ok
-    fun2() # WAS bug
+    fun2() # also works
 
   template foo2() = discard # distractor (template or other symbol kind)
   block:
-    template foo2(lhs, typ, expr) =
-      let lhs = expr
+    template foo2(expr) =
+      expr
+
     proc fun1()=
       let a {.foo2.} = 1
+
     template fun2()=
       let a {.foo2.} = 1
-    fun1() # ok
-    when false: # bug: Error: invalid pragma: foo2
-      fun2()
 
-  block: # proc template pragmas don't work in templates
+    fun1() # ok
+    fun2() # also works
+
+  block template_pragmas_for_templates:
     # adapted from $nim/lib/std/private/since.nim
+
     # case without overload
     template since3(version: (int, int), body: untyped) {.dirty.} =
       when (NimMajor, NimMinor) >= version:
         body
-    when false: # bug
-      template fun3(): int {.since3: (1, 3).} = 12
+    
+    template fun3(): int {.since3: (1, 3).} = 12
 
-  block:
-
+  block template_pragmas_for_templates_with_overloads:
+    # case with overload
     template since2(version: (int, int), body: untyped) {.dirty.} =
       when (NimMajor, NimMinor) >= version:
         body
+
     template since2(version: (int, int, int), body: untyped) {.dirty.} =
       when (NimMajor, NimMinor, NimPatch) >= version:
         body
-    when false: # bug
-      template fun3(): int {.since2: (1, 3).} = 12
 
-when true: # D20210801T100514:here
-  from macros import genSym
-  block:
-    template fn() =
-      var ret {.gensym.}: int # must special case template pragmas so it doesn't get confused
-      discard ret
-    fn()
-    static: discard genSym()
+    template fun3(): int {.since2: (1, 3).} = 12
+
+from macros import genSym
+block:
+  template fn() =
+    var ret {.gensym.}: int # special case template pragmas so it doesn't get confused
+    discard ret
+  fn()
+  static: discard genSym()
+
+block:
+  macro foo(x): untyped = x
+  template bar {.pragma.}
+
+  proc a {.bar.} = discard # works
+  proc b {.bar, foo.} = discard # doesn't
