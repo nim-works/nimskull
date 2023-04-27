f7ccfca5602eab302e95a5607b52b1776e0b46bb
Saem Ghani <saemghani+github@gmail.com>
2023-04-27 16:57:48 -0700

    compiler: compile stats on success refactor (#659)
    ## Summary
    
    Compiler states generated on successful compile are now generated in
    `main`. This removes the dependencies on legacy reports and msgs.
    
    ## Details
    
    This is mostly a refactoring undoing poor design after worse, the
    message is now generated and emitted via `main`, removing this code from
    `msgs` and "reports" modules. In addition, to the above fixed a bug
    where VM profiling dump was not produced for unsuccessful compiles.

--- tests/compilerfeatures/tstructured_echo.nim
+++ tests/compilerfeatures/tstructured_echo.nim
@@ -1,10 +1,10 @@
 discard """
 description: "Structured echo message comparison"
 nimoutformat: "sexp"
-cmd: "nim c --filenames=canonical --msgFormat=sexp $file"
+cmd: "nim c --filenames=canonical --msgFormat=sexp --hint:successx:off $file"
 action: compile
 nimout: '''
-(IntEchoMessage :msg "test message")
+(IntEchoMessage :severity Trace :msg "test message" :location nil :reportInst _ :reportFrom _)
 '''
 """
 
