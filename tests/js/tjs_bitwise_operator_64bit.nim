4add75bb64c3b023f42ba1ee942212f95d6e5187
Saem Ghani <saemghani+github@gmail.com>
2023-04-29 20:23:42 -0700

    renabled `js` testing (#676)
    ## Summary
    
    `js` category tests were disabled due to a bug in testament. This change
    enables them. As part of this `jsffi.bindMethod` was found to broken and
    was deleted, the author fundamentally misunderstands JavaScript's `this`
    binding and `bindMethod` just doesn't make sense.
    
    ## Details
    
    The bug in testament was boolean logic which disabled the entire `js`
    category in the `processCategory` proc. After fixing this a few updates
    were required:
    - deleted `bindMethod` the idea is fundamentally wrong
    - fix tests using deleted `table` API `add`
    - fix malformed test spec in `tjs_bitwise_operator_64bit`

--- tests/js/tjs_bitwise_operator_64bit.nim
+++ tests/js/tjs_bitwise_operator_64bit.nim
@@ -5,8 +5,8 @@ description: '''
   . Echos 2.293GiB for C mode but 37568YiB for JS mode.
   . I've investigated a bit, and it seems to be like that because bitwise
     operators in JS are 32-bit, so 1 << 40 gives 256 and not 1099511627776
-  . Changing bitwise move operation to mul and div. ; "
+  . Changing bitwise move operation to mul and div. ; '''
 """
-import strutils
+import std/strutils
 
 doAssert formatSize(2462056448, '.', bpIEC, false) == "2.293GiB"
