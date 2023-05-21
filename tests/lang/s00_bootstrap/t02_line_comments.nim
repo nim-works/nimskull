2c5cd48c7abd917f6ec4204504e430d61782544b
Saem Ghani <saemghani+github@gmail.com>
2023-05-21 15:26:05 -0700

    Grammar in docs
    Co-authored-by: zerbina <100542850+zerbina@users.noreply.github.com>

--- tests/lang/s00_bootstrap/t02_line_comments.nim
+++ tests/lang/s00_bootstrap/t02_line_comments.nim
@@ -2,7 +2,7 @@
 
 # 20 + 30 # all sorts of things can be written here
 
-## this is also a comment it doesn't have to do anything different currntly and
+## this is also a comment. It doesn't have to do anything different currently and
 ## can be thought of as a comment started with a `#` and the next character
 ## happens to be a `#`; these will be used later for comments/remarks vs
 ## documentation that associates itself with other syntactic structures.
