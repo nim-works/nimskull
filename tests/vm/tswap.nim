57d0a1834af578d4226fcdc5aa11133be26c2462
zerbina <100542850+zerbina@users.noreply.github.com>
2023-06-13 20:24:04 +0000

    tests: add tests
    One for the fixed bug, the other for having explicit coverage of the
    "operands of primitive type" case.

--- tests/vm/tswap.nim
+++ tests/vm/tswap.nim
@@ -24,3 +24,43 @@ block:
     swap(s[0], s[2])
 
     doAssert s == [3, 2, 1]
+
+block arguments_with_side_effect:
+  # both argument expressions must only be evaluated once
+  var
+    a = 1
+    b = 2
+    counter = 0
+
+  swap((inc counter; a), (inc counter; b))
+  doAssert a == 2
+  doAssert b == 1
+  doAssert counter == 2
+
+block primitive_type:
+  # make sure that ``swap`` works for operands of primitive type
+  proc swapLocals() =
+    # both operands are locals
+    var
+      a = 1
+      b = 2
+    swap(a, b)
+    doAssert a == 2
+    doAssert b == 1
+
+  swapLocals()
+
+  proc swapFieldAndLocal() =
+    # one operand is a local, the other one a field
+    type Obj = object
+      field: int
+
+    var
+      a = 1
+      b = Obj(field: 2)
+    swap(a, b.field)
+
+    doAssert a == 2
+    doAssert b.field == 1
+
+  swapFieldAndLocal()
