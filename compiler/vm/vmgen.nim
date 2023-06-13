c69ff1f45df602ccfa5e6b997b4bc6afb8a68b30
zerbina <100542850+zerbina@users.noreply.github.com>
2023-06-13 23:49:21 +0200

    vmgen: fix double evaluation of `swap` arguments (#761)
    ## Summary
    
    Fix a bug the VM code generator had where the expression used
    as the first argument was always evaluated twice -- it is now
    only evaluated once.
    
    ## Details
    
    The `lowerSwap` procedure that was used for lowering the `swap`
    call into assignments duplicated the first argument expression,
    resulting in it being evaluated two times.
    
    Usage of `lowerSwap` is replaced with manually emitting the
    bytecode for the assignments, which in addition to fixing the
    bug and making the generated code slightly more efficient (the
    introduced temporary is freed immediately after the swap now),
    also has the benefit of removing both the import dependency on
    `lowering` and `vmgen` needing access to an `IdGenerator`.
    
    `lowerSwap` has no further usage sites, and is thus removed.
    
    ---------
    
    Co-authored-by: Saem Ghani <saemghani+github@gmail.com>

--- compiler/vm/vmgen.nim
+++ compiler/vm/vmgen.nim
@@ -51,9 +51,6 @@ import
     msgs,
     options
   ],
-  compiler/sem/[
-    lowerings
-  ],
   compiler/utils/[
     idioms
   ],
@@ -1810,7 +1807,37 @@ proc genMagic(c: var TCtx; n: PNode; dest: var TDest; m: TMagic) =
     c.freeTemp(d)
   of mSwap:
     unused(c, n, dest)
-    c.gen(lowerSwap(c.graph, n, c.idgen, if c.prc == nil or c.prc.sym == nil: c.module else: c.prc.sym))
+    let tmp = getTemp(c, n.typ)
+    # XXX: swap doesn't need to be implemented here; lower it into assignment
+    #      with a MIR pass
+    # there's no ``nkHiddenAddr`` on the operands, so we don't need to skip
+    # var types
+    if fitsRegister(n[1].typ):
+      # we need to account for the fact that either operand could be
+      # stored in a register already (because it's a local variable)
+      let
+        a = c.genLoc(n[1])
+        b = c.genLoc(n[2])
+      # register copies are enough, here
+      c.gABC(n, opcFastAsgnComplex, tmp, a.val)
+      c.gABC(n, opcFastAsgnComplex, a.val, b.val)
+      c.gABC(n, opcFastAsgnComplex, b.val, tmp)
+      # write back (if necessary):
+      finish(c, n, b)
+      finish(c, n, a)
+    else:
+      let
+        a = c.genLvalue(n[1])
+        b = c.genLvalue(n[2])
+      # XXX: this currently creates a full copy; the VM is still missing the
+      #      ability to create shallow copies
+      c.gABC(n, opcAsgnComplex, tmp, a)
+      c.gABC(n, opcWrLoc, a, b)
+      c.gABC(n, opcWrLoc, b, tmp)
+      c.freeTemp(b)
+      c.freeTemp(a)
+
+    c.freeTemp(tmp)
   of mIsNil: genUnaryABC(c, n, dest, opcIsNil)
   of mParseBiggestFloat:
     if dest.isUnset: dest = c.getTemp(n.typ)
