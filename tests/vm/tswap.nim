72153e9ed92cda963e40da4111f8b8b5f4924290
zerbina <100542850+zerbina@users.noreply.github.com>
2023-06-13 20:24:03 +0000

    tests: clean up the `tswap.nim` test
    * reference the issues via a full link
    * replace echo with `doAssert`
    * use the VM target

--- tests/vm/tswap.nim
+++ tests/vm/tswap.nim
@@ -1,34 +1,26 @@
 discard """
-nimout: '''
-x.data = @[10]
-y = @[11]
-x.data = @[11]
-y = @[10]
-@[3, 2, 1]
-'''
+  targets: "vm"
 """
 
-# bug #2946
+# https://github.com/nim-lang/nim/issues/2946
 
-proc testSwap(): int {.compiletime.} =
+proc testSwap(): int =
   type T = object
     data: seq[int]
   var x: T
   x.data = @[10]
   var y = @[11]
-  echo "x.data = ", x.data
-  echo "y = ", y
   swap(y, x.data)
-  echo "x.data = ", x.data
-  echo "y = ", y
+  doAssert x.data == @[11]
+  doAssert y == @[10]
   result = 99
 
-const something = testSwap()
+discard testSwap()
 
-# bug #15463
+# https://github.com/nim-lang/nim/issues/15463
 block:
   static:
     var s = @[1, 2, 3]
     swap(s[0], s[2])
 
-    echo s
+    doAssert s == [3, 2, 1]
