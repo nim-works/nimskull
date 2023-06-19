c23b0117874e62f9c51afd50d6bdf4f61411067b
Cristian Camilo Ruiz <mrgaturus@gmail.com>
2023-06-18 23:56:57 -0500

    tests: improve talloc_with_ffi comprehend

--- tests/system/talloc_with_ffi.nim
+++ tests/system/talloc_with_ffi.nim
@@ -1,74 +1,84 @@
 discard """
   targets: "c"
+  description: '''
+    Using the higher-level allocation procedures with types that have a size not
+    known at the NimSkull side must work using backend sizeof
+  '''
 """
 
-type
-  # Imported C Types as Opaques
-  M128* {.importc: "__m128", header: "xmmintrin.h".} = object
-  M128i* {.importc: "__m128i", header: "emmintrin.h".} = object
-  M128d* {.importc: "__m128d", header: "emmintrin.h".} = object
+type FFIObject {.exportc.} = object
+  c, y, o: cint
+# Avoid DCE Optimize Out
+var dummy {.used.}: FFIObject
+
+type 
+  # Imported FFI Type
+  Imported {.importc: "FFIObject", nodecl.} = object
   # Dummy Objects
-  NaivePixel = object
-    r, g, b: int
-  SIMDPixel = object
-    r: M128
-    g: M128i
-    b: M128d
-  TinyPixel = object
-    r {.bitsize: 4.}: cint
-    g {.bitsize: 4.}: cint
+  Native = object
+    a, b: int
+  WithImported = object
+    a, b: int
+    ffi: Imported
+  WithBitField = object
+    a {.bitsize: 4.}: cint
     b {.bitsize: 4.}: cint
-  MixedPixel = object
-    r {.bitsize: 4.}: cint
-    g: int
-    b: M128i
-    a: M128
-  MapNaive = UncheckedArray[NaivePixel]
-  MapSIMD = UncheckedArray[SIMDPixel]
-  MapTiny = UncheckedArray[TinyPixel]
-  MapMixed = UncheckedArray[MixedPixel]
+  Mixed = object
+    a {.bitsize: 4.}: cint
+    b: int
+    c: Imported
+  ArrayNative       = UncheckedArray[Native]
+  ArrayWithImported = UncheckedArray[WithImported]
+  ArrayWithBitField = UncheckedArray[WithBitField]
+  ArrayMixed        = UncheckedArray[Mixed]
 
 block create_simple:
   let
-    a = create(NaivePixel, 2)
-    b = create(SIMDPixel, 2)
-    c = create(TinyPixel, 2)
-    d = create(MixedPixel, 2)
+    a = create(Imported, 2)
+    b = create(Native, 2)
+    c = create(WithImported, 2)
+    d = create(WithBitField, 2)
+    e = create(Mixed, 2)
   # Allocated Successfully?
   doAssert a != nil
   doAssert b != nil
   doAssert c != nil
   doAssert d != nil
+  doAssert e != nil
   # Dealloc Pixels
   dealloc(a)
   dealloc(b)
   dealloc(c)
   dealloc(d)
+  dealloc(e)
 
 block create_shared:
   let
-    a = createShared(NaivePixel, 2)
-    b = createShared(SIMDPixel, 2)
-    c = createShared(TinyPixel, 2)
-    d = createShared(MixedPixel, 2)
+    a = createShared(Imported, 2)
+    b = createShared(Native, 2)
+    c = createShared(WithImported, 2)
+    d = createShared(WithBitField, 2)
+    e = createShared(Mixed, 2)
   # Allocated Successfully?
   doAssert a != nil
   doAssert b != nil
   doAssert c != nil
   doAssert d != nil
+  doAssert e != nil
   # Dealloc Pixels
   dealloc(a)
   dealloc(b)
   dealloc(c)
   dealloc(d)
+  dealloc(e)
 
 block create_unchecked:
-  doAssert not compiles(create(MapNaive))
-  doAssert not compiles(createU(MapSIMD))
-  doAssert not compiles(createShared(MapTiny))
-  doAssert not compiles(createSharedU(MapMixed))
+  doAssert not compiles(create(ArrayNative))
+  doAssert not compiles(createU(ArrayWithImported))
+  doAssert not compiles(createShared(ArrayWithBitField))
+  doAssert not compiles(createSharedU(ArrayMixed))
   # ptr and ref UncheckedArray Should Compile
-  doAssert compiles(create(ref MapMixed))
-  doAssert compiles(createU(ptr MapSIMD))
-  doAssert compiles(createShared(ptr MapTiny))
-  doAssert compiles(createSharedU(ref MapMixed))
+  doAssert compiles(create(ref ArrayNative))
+  doAssert compiles(createU(ptr ArrayWithImported))
+  doAssert compiles(createShared(ptr ArrayWithBitField))
+  doAssert compiles(createSharedU(ref ArrayMixed))
