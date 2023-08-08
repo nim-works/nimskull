729e1c74a1d43a65134538f6fb6ae64b722f1634
Saem Ghani <saemghani+github@gmail.com>
2023-08-08 14:18:13 -0700

    make tests gc independent
    define a type hook to force a definition, rather than relying on the gc being used to coerce it into happening.
    
    Co-authored-by: zerbina <100542850+zerbina@users.noreply.github.com>

--- tests/lang_objects/destructor/ttypeops_misc.nim
+++ tests/lang_objects/destructor/ttypeops_misc.nim
@@ -5,9 +5,15 @@ block ensure_lifting_is_not_confused_by_type_wrappers:
   ## previously this led to a code gen bug where a non-existent hidden type
   ## field was being copied. the various wrappers alias/distinct weren't being
   ## skipped when testing to see if the underlying type was an object.
+  type WithHooks = object
+
+  proc `=destroy`(x: var WithHooks) =
+    # force `WithHooks` to require lifetime hooks
+    discard
+
   type
     Object = object
-      x: string # force `Object` to require lifetime hooks
+      x: WithHooks # make sure that `Object` gets a synthesized copy hook
     Alias = Object
     Distinct = distinct Alias
 
