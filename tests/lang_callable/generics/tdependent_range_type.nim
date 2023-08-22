71841afe2669d79aa0c070557e211ce30fcee4bc
zerbina <100542850+zerbina@users.noreply.github.com>
2023-08-22 22:24:28 +0100

    tests: add a test for ranges
    Doesn't work yet, due to unrelated issues with `typeRel`.

---
+++ tests/lang_callable/generics/tdependent_range_type.nim
@@ -0,0 +1,39 @@
+discard """
+  action: compile
+  description: '''
+    Range types depending on type variables must work in routine signatures
+  '''
+  knownIssue: '''
+    ``typeRel`` doesn't properly handle the case where a formal range type is
+    depends on a type variables
+  '''
+"""
+
+import std/typetraits
+
+proc typeNameLen(x: typedesc): int =
+  x.name.len
+
+proc f(t: typedesc, rng: range[-typeNameLen(t) .. typeNameLen(t)]): int =
+  result = rng.type.high - rng.type.low
+
+type
+  MyType = object # only provides the name
+  Typ    = object # shorter name
+
+static:
+  # test non-range argument type:
+  doAssert f(MyType, 0) == 12
+
+  # check that the formal range is available when computing the type
+  # relationship formal and argument type:
+  var a: range[4..8] # overlapping range -> matches
+  doAssert f(MyType, a) == 12
+
+  var b: range[10..12] # doesn't overlap -> no match
+  doAssert not compiles(f(MyType, b) == 12)
+
+  # test with a different name:
+  doAssert f(Typ, 0) == 6
+  doAssert not compiles(f(Typ, 5) == 6)
+  doAssert not compiles(f(Typ, a) == 6)
