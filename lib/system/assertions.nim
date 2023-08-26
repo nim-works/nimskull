f03b0da6a2dccd816f3409015bbb3cef1d094b4e
bung87 <crc32@qq.com>
2023-08-27 02:00:08 +0800

    follow suggestions

--- lib/system/assertions.nim
+++ lib/system/assertions.nim
@@ -44,7 +44,7 @@ template assertImpl(cond: bool, msg: string, expr: string, enabled: static[bool]
       ploc = $loc
     bind instantiationInfo
     mixin failedAssertImpl
-    {.line: instantiationInfo(fullPaths = true).}:
+    {.line.}:
       if not cond:
         failedAssertImpl(ploc & " `" & expr & "` " & msg)
 
