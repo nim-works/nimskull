47e33d159a8d6a64289e3c6b5f1afb5212bd92bd
bung87 <crc32@qq.com>
2023-08-27 02:26:43 +0800

    should use instantiationInfo(fullPaths = true), stacktrace different

--- lib/system/assertions.nim
+++ lib/system/assertions.nim
@@ -44,7 +44,7 @@ template assertImpl(cond: bool, msg: string, expr: string, enabled: static[bool]
       ploc = $loc
     bind instantiationInfo
     mixin failedAssertImpl
-    {.line.}:
+    {.line: instantiationInfo(fullPaths = true).}:
       if not cond:
         failedAssertImpl(ploc & " `" & expr & "` " & msg)
 
