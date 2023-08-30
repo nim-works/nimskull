de0eb7f4524a233e0242f55a31d4177793c3a51a
Saem Ghani <saemghani+github@gmail.com>
2023-08-29 19:34:14 -0700

    specify tests target to avoid failures

--- tests/lang_callable/overload/tuntyped_param_not_followed_by_typed.nim
+++ tests/lang_callable/overload/tuntyped_param_not_followed_by_typed.nim
@@ -1,13 +1,13 @@
 discard """
 description: "Ensures untyped params aren't followed by typed ones"
 cmd: "nim check --hints:off $file"
+targets: "c"
 nimout: '''
 tuntyped_param_not_followed_by_typed.nim(15, 29) Warning: non-untyped param must not appear after an untyped param [rsemUntypedParamsFollwedByMoreSpecificType]
 tuntyped_param_not_followed_by_typed.nim(18, 37) Warning: non-untyped param must not appear after an untyped param [rsemUntypedParamsFollwedByMoreSpecificType]
 tuntyped_param_not_followed_by_typed.nim(21, 46) Warning: non-untyped param must not appear after an untyped param [rsemUntypedParamsFollwedByMoreSpecificType]
 '''
 """
-
 
 
 
