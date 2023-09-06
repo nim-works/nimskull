45184ae3aed157d5f667f807de67b961eaa42cab
zerbina <100542850+zerbina@users.noreply.github.com>
2023-09-06 13:40:54 +0000

    liftdestructors: don't canonicalize object types
    With each unique object type now having a separate `PType` instance,
    object types don't have to be canonicalized anymore. This reduces the
    amount of work for `createTypeBoundOps`, and there's also less types in
    the `canonTypes` table.

--- compiler/sem/liftdestructors.nim
+++ compiler/sem/liftdestructors.nim
@@ -996,11 +996,21 @@ proc createTypeBoundOps(g: ModuleGraph; c: PContext; orig: PType; info: TLineInf
   let skipped = orig.skipTypes({tyGenericInst, tyAlias, tySink})
   if isEmptyContainer(skipped) or skipped.kind == tyStatic: return
 
-  let h = sighashes.hashType(skipped, {CoType, CoDistinct})
-  var canon = g.canonTypes.getOrDefault(h)
-  if canon == nil:
-    g.canonTypes[h] = skipped
+  var canon: PType
+  if skipped.kind == tyObject:
+    # for nominal types, the type itself is already the canonical one (each one
+    # is unique)
+    # XXX: ^^ at present, this is only true for object types. Phantom
+    #      ``tyDistinct`` and ``tyEnum`` types still don't have unique
+    #      instances
     canon = skipped
+  else:
+    # structural types use canonicalization
+    let h = sighashes.hashType(skipped, {CoType, CoDistinct})
+    canon = g.canonTypes.getOrDefault(h)
+    if canon == nil:
+      g.canonTypes[h] = skipped
+      canon = skipped
 
   # multiple cases are to distinguish here:
   # 1. we don't know yet if 'typ' has a nontrival destructor.
