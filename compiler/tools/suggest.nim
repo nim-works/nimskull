269890e694ca56395af90cef0c4812a9d4f6c070
Bung <crc32@qq.com>
2023-09-17 03:12:50 +0800

    refactoring suggest hooks, Config.writeHook default to msgs.msgWrite (#902)
    <!--- The Pull Request (=PR) message is what will get automatically used
    as
    the commit message when the PR is merged. Make sure that no line is
    longer
    than 72 characters -->
    
    ## Summary
    
    Remove `msgs.suggestWriteln` and unnecessary callback wrapper for
    `ConfigRef.writeHook` registration.
    
    ## Details 
    * Remove procedure `suggestWriteln` and its call in `suggest`,
    `suggestWriteln` wraps around `writelnHook`, `writelnHook` original 
    purpose is write message to stdio or some like that, not for suggest
    result.
    * `config.writeHook` assign to `msgs.msgWrite` directly in `nim.nim` and
      `vmrunner.nim`, now not a wrapper procedure  around it.

--- compiler/tools/suggest.nim
+++ compiler/tools/suggest.nim
@@ -247,8 +247,6 @@ proc `$`*(suggest: Suggest): string =
 proc suggestResult(conf: ConfigRef; s: Suggest) =
   if not isNil(conf.suggestionResultHook):
     conf.suggestionResultHook(s)
-  else:
-    conf.suggestWriteln($s)
 
 proc produceOutput(a: var Suggestions; conf: ConfigRef) =
   if conf.ideCmd in {ideSug, ideCon}:
@@ -260,9 +258,6 @@ proc produceOutput(a: var Suggestions; conf: ConfigRef) =
   if not isNil(conf.suggestionResultHook):
     for s in a:
       conf.suggestionResultHook(s)
-  else:
-    for s in a:
-      conf.suggestWriteln($s)
 
 proc filterSym(s: PSym; prefix: PNode; res: var PrefixMatch): bool {.inline.} =
   proc prefixMatch(s: PSym; n: PNode): PrefixMatch =
