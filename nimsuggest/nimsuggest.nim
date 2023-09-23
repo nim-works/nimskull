c346db4efed224d9d30c7e61c61f108292263464
bung87 <crc32@qq.com>
2023-09-23 23:29:50 +0800

    remove unrelated changes

--- nimsuggest/nimsuggest.nim
+++ nimsuggest/nimsuggest.nim
@@ -775,9 +775,6 @@ else:
     myLog(conf, "START " & conf.projectFull.string)
 
     var graph = newModuleGraph(cache, conf)
-    graph.onMarkUsed = proc (g: ModuleGraph; info: TLineInfo; s: PSym; usageSym: var PSym; isDecl: bool) =
-      suggestSym(g, info, s, usageSym, isDecl)
-    graph.onSymImport = graph.onMarkUsed # same callback
     if self.loadConfigsAndProcessCmdLine(cache, conf, graph):
       mockCommand(graph)
     if gLogging:
