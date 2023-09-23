e52070fc08d8e57b513cf5f9dafd79f43059caac
bung87 <crc32@qq.com>
2023-09-23 23:03:17 +0800

    add more test cases

--- nimsuggest/tests/timport_multi_modules.nim
+++ nimsuggest/tests/timport_multi_modules.nim
@@ -1,12 +1,17 @@
 discard """
 $nimsuggest --tester $file
 >def $1
-def;;skModule;;minclude_types;;*;;$file;;10;;15;;"";;100
+def;;skModule;;minclude_types;;*;;$file;;15;;15;;"";;100
 >def $2
-def;;skModule;;minclude_types;;*;;$file;;11;;15;;"";;100
+def;;skModule;;minclude_types;;*;;$file;;16;;15;;"";;100
 >def $3
-def;;skModule;;minclude_import;;*;;$file;;13;;10;;"";;100
+def;;skModule;;minclude_import;;*;;$file;;18;;10;;"";;100
+>def $4
+def;;skModule;;minclude_types;;*;;$file;;21;;13;;"";;100
+>def $5
+def;;skModule;;mstrutils;;*;;$file;;22;;15;;"";;100
 """
+
 import fixtures/[minclude_import, minclud#[!]#e_types]
 import fixtures/[minclude_types as typ#[!]#es]
 # test with multi-line import statements:
@@ -14,3 +19,5 @@ import
   fixtures/[
     minclu#[!]#de_import
   ]
+from fixtures/mincl#[!]#ude_types import Greet
+import fixtures/mst#[!]#rutils except replace
