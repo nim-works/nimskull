9afa9145713cc4b43d8ce23ff6d6d0e84fbb529b
bung87 <crc32@qq.com>
2023-09-23 14:43:40 +0800

    add test case

---
+++ nimsuggest/tests/tdef_imports.nim
@@ -0,0 +1,16 @@
+discard """
+$nimsuggest --tester $file
+>def $1
+def;;skModule;;strutils;;;;$file;;11;;10;;*;;100
+>def $2
+def;;skModule;;strformat;;;;$file;;12;;10;;*;;100
+>def $3
+def;;skModule;;strutils;;;;$file;;15;;5;;*;;100
+"""
+
+import std/[strformat, stru#[!]#tils]
+import std/[strformat as sfor#[!]#mat]
+import
+  std/[
+    s#[!]#trutils
+  ]
