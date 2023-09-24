9fd2b3976491f8b3ce3610dbe6403acbca7309f5
bung87 <crc32@qq.com>
2023-09-24 11:26:44 +0800

    remove unrelated changes

--- nimlsp/nimlsppkg/nimsuggest.nim
+++ nimlsp/nimlsppkg/nimsuggest.nim
@@ -127,114 +127,6 @@ proc initNimSuggest*(project: string, nimPath: string = ""): NimSuggest =
   retval.doStopCompile = proc (): bool = false
   return NimSuggest(graph: retval, idle: 0, cachedMsgs: cachedMsgs)
 
-proc getSymNode(node: ParsedNode): ParsedNode =
-  result = node
-  if result.kind == pnkPostfix:
-    result = result[^1]
-  elif result.kind == pnkPragmaExpr:
-    result = getSymNode(result[0])
-
-proc pnkToSymKind(kind: ParsedNodeKind): TSymKind =
-  result = skUnknown
-  case kind
-  of pnkConstSection, pnkConstDef: result = skConst
-  of pnkLetSection: result = skLet
-  of pnkVarSection: result = skVar
-  of pnkProcDef: result = skProc
-  of pnkFuncDef: result = skFunc
-  of pnkMethodDef: result = skMethod
-  of pnkConverterDef: result = skConverter
-  of pnkIteratorDef: result = skIterator
-  of pnkMacroDef: result = skMacro
-  of pnkTemplateDef: result = skTemplate
-  of pnkTypeDef, pnkTypeSection: result = skType
-  else: discard
-
-proc getName(node: ParsedNode): string =
-  if node.kind == pnkIdent:
-    result = node.startToken.ident.s
-  elif node.kind == pnkAccQuoted:
-    result = "`"
-    for t in node.idents:
-      result.add t.ident.s
-    result.add "`"
-
-proc processFlags(sug: Suggest; n: ParsedNode) =
-  var
-    identDeprecated: bool
-    colonDeprecated: bool
-  for s in n.sons:
-    identDeprecated = s.kind == pnkIdent and getName(s) == "deprecated"
-    colonDeprecated = s.kind == pnkExprColonExpr and getName(s[0]) == "deprecated" 
-    if identDeprecated or colonDeprecated:
-      sug.flags.incl SuggestFlag.deprecated
-
-proc parsedNodeToSugget(n: ParsedNode; originKind: ParsedNodeKind; module: string): Suggest =
-  if n.kind in {pnkError, pnkEmpty}: return
-  if n.kind notin {pnkProcDef..pnkVarTuple}: return
-  new(result)
-  var token = getToken(n)
-  var name = ""
-
-  if n.kind in pnkRoutineDefs and n[pragmasPos].kind == pnkPragma:
-    processFlags(result, n[pragmasPos])
-  elif n[0].kind == pnkPragmaExpr and n[0][^1].kind == pnkPragma:
-    processFlags(result, n[0][^1])
-
-  if n.kind != pnkVarTuple:
-    var node: ParsedNode = getSymNode(n[0])
-    token = getToken(node)
-    if node.kind != pnkError:
-      name = getName(node)
-      when false:
-        if n.kind in pnkRoutineDefs and node.kind == pnkAccQuoted:
-          let identsLen = n[paramsPos].sons.len
-          for i in countup(1, identsLen - 1):
-            name.add getName(n[paramsPos][i][1])
-            if i != identsLen - 1:
-              name.add ","
-  else:
-    name.add "("
-    for c in n.sons:
-      if c.kind == pnkEmpty: break
-      name.add getName(c) & ","
-    name.add ")"
-  if name != "":
-    result.qualifiedPath = @[module, name]
-  result.line = token.line.int
-  result.column = token.col.int
-  result.tokenLen = name.len
-  result.symkind = byte pnkToSymKind(originKind)
-
-proc outline(graph: ModuleGraph; fileIdx: FileIndex) =
-  let conf = graph.config
-  var parser: Parser
-  var sug: Suggest
-  var parsedNode: ParsedNode
-  let name = splitFile(toFilename(conf, fileIdx)).name
-
-  const Sections = {pnkTypeSection, pnkConstSection, pnkLetSection}
-  template suggestIt(parsedNode: ParsedNode; originKind: ParsedNodeKind) =
-    sug = parsedNodeToSugget(parsedNode, originKind, name)
-    if sug != nil:
-      sug.section = ideOutline
-      sug.quality = 100
-      sug.filePath = toFullPath(conf, fileIdx)
-      conf.suggestionResultHook(sug)
-
-  if setupParser(parser, fileIdx, graph.cache, conf):
-    while true:
-      parsedNode = parser.parseTopLevelStmt()
-      if parsedNode.kind == pnkEmpty:
-        break
-
-      if parsedNode.kind in Sections:
-        for node in parsedNode.sons:
-          suggestIt(node, parsedNode.kind)
-      else:
-        suggestIt(parsedNode, parsedNode.kind)
-    closeParser(parser)
-
 proc executeNoHooks(cmd: IdeCmd, file, dirtyfile: AbsoluteFile, line, col: int,
              graph: ModuleGraph) =
   let conf = graph.config
@@ -245,9 +137,6 @@ proc executeNoHooks(cmd: IdeCmd, file, dirtyfile: AbsoluteFile, line, col: int,
       listUsages(graph, u)
     else:
       stderr.writeLine "found no symbol at position: " & (conf $ conf.m.trackPos)
-  elif conf.ideCmd == ideOutline:
-    let dirtyIdx = fileInfoIdx(conf, file)
-    outline(graph, dirtyIdx)
 
 proc reportToSuggest(conf: ConfigRef, info: TLineInfo, r: Report): Suggest =
   Suggest(section: ideChk, filePath: toFullPath(conf, info),
