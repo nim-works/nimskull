f04e4bb2acca591541dcb15e9b61bf37d9f38536
Bung <crc32@qq.com>
2023-10-01 05:50:57 +0800

    Update tests/lang_callable/macros/tif_ill_formed.nim
    Co-authored-by: zerbina <100542850+zerbina@users.noreply.github.com>

--- tests/lang_callable/macros/tif_ill_formed.nim
+++ tests/lang_callable/macros/tif_ill_formed.nim
@@ -12,6 +12,6 @@ macro t1(): untyped =
   result =
     nnkIfStmt.newTree(
       newNimNode(nnkElseExpr)) #[tt.Error
-               ^ illformed AST:  else: <<0th child missing for nkElseExpr >>]#
+                ^ illformed AST:  else: <<0th child missing for nkElseExpr >>]#
 
 t1()
