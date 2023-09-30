b2205bb07684fdd078c4afe631658a577644be9e
bung87 <crc32@qq.com>
2023-10-01 00:26:43 +0800

    add test case for if branch has wrong length

---
+++ tests/lang_callable/macros/tif_ill_formed.nim
@@ -0,0 +1,16 @@
+discard """
+  cmd: "nim check $options $file"
+  action: reject
+  nimout: '''
+tif_ill_formed.nim(13, 15) Error: illformed AST:  else: <<0th child missing for nkElseExpr >>
+'''
+"""
+import std/macros
+
+macro t1(): untyped =
+  result = newTree(nnkIfStmt)
+  result.add(
+    newNimNode(nnkElseExpr)
+  )
+
+t1()
