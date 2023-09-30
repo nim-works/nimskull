6e0f97db214e4f93bb702c80a7a075d3c514496c
Bung <crc32@qq.com>
2023-10-01 04:58:02 +0800

    Update tests/lang_callable/macros/tif_ill_formed.nim
    Co-authored-by: zerbina <100542850+zerbina@users.noreply.github.com>

--- tests/lang_callable/macros/tif_ill_formed.nim
+++ tests/lang_callable/macros/tif_ill_formed.nim
@@ -2,8 +2,8 @@ discard """
   description: '''
     Ensure that ill-formed if-statement AST created by macros is detected
   '''
-  cmd: "nim check --msgFormat:sexp --filenames=canonical --hints:off $options $file"
-  nimoutformat: sexp
+  cmd: "nim check --hints:off $options $file"
+  nimoutfull: "true"
   action: reject
 """
 import std/macros
