5fe7805538ab296ca61a74495cfb3f8d2199f867
bung87 <crc32@qq.com>
2023-10-03 01:28:42 +0800

    fix hasCom miss case by replacing nkEmpty..nkNilLit to nkWithoutSons

--- compiler/ast/renderer.nim
+++ compiler/ast/renderer.nim
@@ -535,7 +535,7 @@ proc hasCom(n: PNode): bool =
   if n.isNil: return false
   if n.comment.len > 0: return true
   case n.kind
-  of nkEmpty..nkNilLit: discard
+  of nkWithoutSons: discard
   else:
     for i in 0..<n.len:
       if hasCom(n[i]): return true
