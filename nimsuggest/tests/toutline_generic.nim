12aa4d4f7cec8352ca7eb94fc39f5999caea3a4c
Bung <crc32@qq.com>
2023-10-07 11:55:45 +0800

    Update nimsuggest/tests/toutline_generic.nim
    Co-authored-by: zerbina <100542850+zerbina@users.noreply.github.com>

--- nimsuggest/tests/toutline_generic.nim
+++ nimsuggest/tests/toutline_generic.nim
@@ -1,11 +1,27 @@
-from typetraits import supportsCopyMem
+# Regression test for symbols within template and generic bodies being
+# wrongfully reported by `outline`.
 
-proc tgeneric[T](x: var T) =
-  when supportsCopyMem(T):
-    discard
+proc generic[T]() =
+  var x = 0 #[!]#
+  return # <- when no cursor is provided, this triggered `x` being reported
+
+template templ() =
+  var x = 0 #[!]#
+  return # <- when no cursor is provided, this triggered `x` being reported
+
+# try both with and without a specified cursor position
 
 discard """
 $nimsuggest --tester $file
+>outline $1
+outline;;skProc;;toutline_generic.generic;;*;;4;;5;;"";;100
+outline;;skTemplate;;toutline_generic.templ;;*;;9;;9;;"";;100
+
+>outline $2
+outline;;skProc;;toutline_generic.generic;;*;;4;;5;;"";;100
+outline;;skTemplate;;toutline_generic.templ;;*;;9;;9;;"";;100
+
 >outline $path/toutline_generic.nim
-outline;;skProc;;toutline_generic.tgeneric;;*;;3;;5;;"";;100
+outline;;skProc;;toutline_generic.generic;;*;;4;;5;;"";;100
+outline;;skTemplate;;toutline_generic.templ;;*;;8;;9;;"";;100
 """
