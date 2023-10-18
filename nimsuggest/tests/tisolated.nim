6a11f1949e545f16e827b5df8cf121629e0a7ef7
Bung <crc32@qq.com>
2023-10-18 10:59:41 +0800

    Update nimsuggest/tests/tisolated.nim
    Co-authored-by: Saem Ghani <saemghani+github@gmail.com>

--- nimsuggest/tests/tisolated.nim
+++ nimsuggest/tests/tisolated.nim
@@ -1,8 +1,11 @@
-# Regression test for server initialazed with project file but query another
-# file that not indexed with project's graph. this covers below use cases
-# 1. a new file under project directory structure not be imported nor included
-#    by compiled files.
-# 2. a file that used by explicitly importing in another project.
+# Regression test for querying a file/module that is not the project file/module
+# or part of the transitive closure of all dependencies (via import or include).
+# In such a case the queried file is not indexed within the project's graph.
+#
+# Covered use cases:
+# 1. a new file under project directory structure not imported nor included by
+#    compiled files.
+# 2. a file that's used by explicitly importing in another project.
 
 discard """
 $nimsuggest --tester $file
