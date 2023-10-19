8175c29dfa5af2827cd31a6f83c3fddd65e49202
Clyybber <darkmine956@gmail.com>
2023-10-19 17:49:09 +0200

    Remove unused nodeflags and move TNode id field
    this decreases the size of TNode on x86_64 from 48 to 40 bytes

--- compiler/ast/ast_types.nim
+++ compiler/ast/ast_types.nim
@@ -604,8 +604,6 @@ type
     nfDotField  ## the call can use a dot operator
     nfDotSetter ## the call can use a setter dot operarator
     nfExplicitCall ## `x.y()` was used instead of x.y
-    nfIsRef     ## this node is a 'ref' node; used for the VM
-    nfIsPtr     ## this node is a 'ptr' node; used for the VM
     nfFromTemplate ## a top-level node returned from a template
     nfDefaultParam ## an automatically inserter default parameter
     nfDefaultRefsParam ## a default param value references another parameter
@@ -1535,8 +1533,8 @@ type
         discard
 
   TNode*{.final, acyclic.} = object # on a 32bit machine, this takes 32 bytes
-    id*: NodeId
     typ*: PType
+    id*: NodeId
     info*: TLineInfo
     flags*: TNodeFlags
     case kind*: TNodeKind
