nim-works/nimskull

commit dc2ff8a4c49f3eb57ce11d84aeb5d33aa46b5f42

rework the MIR (part 1) (#942)
## Summary

Start reworking the MIR by making it more imperative and changing it to
a fully expression/statement-based IR. This brings the MIR closer to
the `CgNode` IR, preparing for the eventual replacement of the latter
with the former.

As a side-effect of the IR changes, JavaScript FFI procedures that
rely on expression shapes rather than types in order to behave
correctly no longer work in most cases. In other words, FFI procedure
cannot be used like templates anymore.

Multiple bugs are fixed, but some regressions are also introduced
(although only temporarily). The most significant fixes are:
* the strict left-to-right, inside-to-outside evaluation order is now
  consistently enforced across all backends
* values moved into `sink` parameter or aggregate construction are no
  longer leaked when executing a following argument expression leaves
  the call/construction expression
* exiting a `ref`-object construction expression through unstructured
  control-flow no longer leaks the already-allocated heap cell
* calling `proc f(x: T, y: sink T): lent T` with `f(a, a)` (the
  arguments both name the same location) prevents `a` from being sunk
  into the parameter, thus preventing the value from being freed too
  early

## Details

### Overview

At the core of the rework's first part is replacing the previous stack-
machine-like representation of the MIR with a fully statement-and-
expression-based one that's very similar to the `CgNode` IR, but with
some important differences:
* the MIR (still) uses a flat in-memory representation rather than a
  `ref`-tree
* expressions in the MIR do not contain statements
* rvalue expressions (calls, constructions, conversions, casts, etc.)
  only appear as the source operand of assignments

Compared to the previous design, the new MIR is:
* easier to use for the code generators
* easier to extend
* more flexible and easier to transform/lower

compiler/backend/ccgcalls.nim
@@ -131,13 +131,8 @@ proc fixupCall(p: BProc, le, ri: CgNode, d: var TLoc,
 proc genBoundsCheck(p: BProc; arr, a, b: TLoc)
 
 proc reifiedOpenArray(p: BProc, n: CgNode): bool {.inline.} =
-  var x = n
-  while x.kind in {cnkAddr, cnkHiddenAddr, cnkHiddenConv, cnkDerefView}:
-    x = x.operand
-  if x.kind == cnkLocal and p.locals[x.local].k == locParam:
-    result = false
-  else:
-    result = true
+  # all non-parameter openArrays are reified
+  not(n.kind == cnkLocal and p.locals[n.local].k == locParam)
 
 proc genOpenArraySlice(p: BProc; q: CgNode; formalType, destType: PType): (Rope, Rope) =
   var a, b, c: TLoc
@@ -183,71 +178,28 @@ proc genOpenArraySlice(p: BProc; q: CgNode; formalType, destType: PType): (Rope,
   else:
     internalError(p.config, "openArrayLoc: " & typeToString(a.t))
 
-proc openArrayLoc(p: BProc, formalType: PType, n: CgNode): Rope =
-  var q = skipConv(n)
-  var skipped = false
-  while q.kind == cnkStmtListExpr and q.len > 0:
-    skipped = true
-    q = q.lastSon
-  if getMagic(q) == mSlice:
-    # magic: pass slice to openArray:
-    if skipped:
-      q = skipConv(n)
-      while q.kind == cnkStmtListExpr and q.len > 0:
-        for i in 0..<q.len-1:
-          genStmts(p, q[i])
-        q = q.lastSon
-    let (x, y) = genOpenArraySlice(p, q, formalType, n.typ[0])
-    result = x & ", " & y
-  else:
-    var a: TLoc
-    initLocExpr(p, if n.kind == cnkHiddenConv: n.operand else: n, a)
-    case skipTypes(a.t, abstractVar+{tyStatic}).kind
-    of tyOpenArray, tyVarargs:
-      if reifiedOpenArray(p, n):
-        result = "$1.Field0, $1.Field1" % [rdLoc(a)]
-      else:
-        result = "$1, $1Len_0" % [rdLoc(a)]
-    of tyString, tySequence:
-      let ntyp = skipTypes(n.typ, abstractInst)
-      if formalType.skipTypes(abstractInst).kind in {tyVar} and ntyp.kind == tyString:
-        linefmt(p, cpsStmts, "#nimPrepareStrMutationV2($1);$n", [byRefLoc(p, a)])
-      if ntyp.kind in {tyVar}:
-        var t: TLoc
-        t.r = "(*$1)" % [a.rdLoc]
-        result = "(*$1)$3, $2" % [a.rdLoc, lenExpr(p, t), dataField(p)]
-      else:
-        result = "$1$3, $2" % [a.rdLoc, lenExpr(p, a), dataField(p)]
-    of tyArray:
-      result = "$1, $2" % [rdLoc(a), rope(lengthOrd(p.config, a.t))]
-    of tyPtr, tyRef:
-      case lastSon(a.t).kind
-      of tyString, tySequence:
-        var t: TLoc
-        t.r = "(*$1)" % [a.rdLoc]
-        result = "(*$1)$3, $2" % [a.rdLoc, lenExpr(p, t), dataField(p)]
-      of tyArray:
-        result = "$1, $2" % [rdLoc(a), rope(lengthOrd(p.config, lastSon(a.t)))]
-      else:
-        internalError(p.config, "openArrayLoc: " & typeToString(a.t))
-    else: internalError(p.config, "openArrayLoc: " & typeToString(a.t))
-
 proc literalsNeedsTmp(p: BProc, a: TLoc): TLoc =
   getTemp(p, a.lode.typ, result)
   genAssignment(p, result, a)
 
-proc genArgStringToCString(p: BProc, n: CgNode): Rope {.inline.} =
-  var a: TLoc
-  initLocExpr(p, n.operand, a)
-  ropecg(p.module, "#nimToCStringConv($1)", [rdLoc(a)])
-
 proc genArg(p: BProc, n: CgNode, param: PSym; call: CgNode): Rope =
   var a: TLoc
-  if n.kind == cnkStringToCString:
-    result = genArgStringToCString(p, n)
-  elif skipTypes(param.typ, abstractVar).kind in {tyOpenArray, tyVarargs}:
-    var n = if n.kind != cnkHiddenAddr: n else: n.operand
-    result = openArrayLoc(p, param.typ, n)
+  if skipTypes(param.typ, abstractVar).kind in {tyOpenArray, tyVarargs}:
+    # openArray parameters are translated to two C parameter: one for the
+    # pointer, another for the length
+    initLocExpr(p, n, a)
+    if n.typ.skipTypes(abstractInst + tyUserTypeClasses).kind == tyArray:
+      # FIXME: array values must not be passed to openArray parameters
+      #        directly, but the required ``nkHiddenStdConv``/
+      #        ``nkHiddenSubConv`` is not produced by sem in the following
+      #        case:
+      #          proc f(x: varargs[int]) = ...
+      #          f(x = 0)
+      result = "$1, $2" % [rdLoc(a), rope(lengthOrd(p.config, a.t))]
+    elif reifiedOpenArray(p, n):
+      result = "$1.Field0, $1.Field1" % [rdLoc(a)]
+    else:
+      result = "$1, $1Len_0" % [rdLoc(a)]
   elif ccgIntroducedPtr(p.config, param, call[0].typ[0]):
     initLocExpr(p, n, a)
     if n.kind in cnkLiterals + {cnkNilLit}:
@@ -261,11 +213,8 @@ proc genArg(p: BProc, n: CgNode, param: PSym; call: CgNode): Rope =
 
 proc genArgNoParam(p: BProc, n: CgNode, needsTmp = false): Rope =
   var a: TLoc
-  if n.kind == cnkStringToCString:
-    result = genArgStringToCString(p, n)
-  else:
-    initLocExprSingleUse(p, n, a)
-    result = rdLoc(a)
+  initLocExprSingleUse(p, n, a)
+  result = rdLoc(a)
 
 proc genParams(p: BProc, ri: CgNode, typ: PType): Rope =
   for i in 1..<ri.len:

compiler/backend/ccgexprs.nim
@@ -136,7 +136,7 @@ proc genSetNode(p: BProc, n: CgNode): Rope =
 proc genOpenArrayConv(p: BProc; d: TLoc; a: TLoc) =
   assert d.k != locNone
 
-  case a.t.skipTypes(abstractVar).kind
+  case a.t.skipTypes(abstractVar + tyUserTypeClasses).kind
   of tyOpenArray, tyVarargs:
     if reifiedOpenArray(p, a.lode):
       linefmt(p, cpsStmts, "$1.Field0 = $2.Field0; $1.Field1 = $2.Field1;$n",
@@ -151,7 +151,7 @@ proc genOpenArrayConv(p: BProc; d: TLoc; a: TLoc) =
     linefmt(p, cpsStmts, "$1.Field0 = $2; $1.Field1 = $3;$n",
       [rdLoc(d), rdLoc(a), rope(lengthOrd(p.config, a.t))])
   of tyString:
-    if skipTypes(a.t, abstractInst).kind in {tyVar}:
+    if skipTypes(d.t, abstractInst).kind in {tyVar}:
       linefmt(p, cpsStmts, "#nimPrepareStrMutationV2($1);$n", [byRefLoc(p, a)])
 
     linefmt(p, cpsStmts, "$1.Field0 = $2$3; $1.Field1 = $4;$n",
@@ -812,16 +812,19 @@ proc genArrayLikeElem(p: BProc; n: CgNode; d: var TLoc) =
   discard getTypeDesc(p.module, n.typ)
 
 proc genEcho(p: BProc, n: CgNode) =
-  # this unusual way of implementing it ensures that e.g. ``echo("hallo", 45)``
-  # is threadsafe.
-  internalAssert p.config, n.kind == cnkArrayConstr
-  block:
-    if n.len == 0:
-      linefmt(p, cpsStmts, "#echoBinSafe(NIM_NIL, $1);$n", [n.len])
-    else:
+  ## Generates and emits the code for the magic echo call.
+  if n.len == 1:
+    linefmt(p, cpsStmts, "#echoBinSafe(NIM_NIL, 0);$n", [])
+  else:
+    # allocate a temporary array and fill it with the arguments:
+    var tmp: TLoc
+    getTemp(p, n[1].typ, tmp) # the first argument stores the type to use
+    for i in 2..<n.len:
       var a: TLoc
-      initLocExpr(p, n, a)
-      linefmt(p, cpsStmts, "#echoBinSafe($1, $2);$n", [a.rdLoc, n.len])
+      initLocExpr(p, n[i], a)
+      linefmt(p, cpsStmts, "$1[$2] = $3;$n", [rdLoc(tmp), i-2, rdLoc(a)])
+
+    linefmt(p, cpsStmts, "#echoBinSafe($1, $2);$n", [rdLoc(tmp), n.len-2])
 
 proc strLoc(p: BProc; d: TLoc): Rope =
   result = byRefLoc(p, d)
@@ -1189,6 +1192,7 @@ proc genSeqConstr(p: BProc, n: CgNode, d: var TLoc) =
 proc genArrToSeq(p: BProc, n: CgNode, d: var TLoc) =
   var elem, a, arr: TLoc
   if n[1].kind == cnkArrayConstr:
+    # XXX: dead code, but kept as a reminder
     n[1].typ = n.typ
     genSeqConstr(p, n[1], d)
     return
@@ -1307,22 +1311,11 @@ template genDollar(p: BProc, n: CgNode, d: var TLoc, frmt: string) =
   genAssignment(p, d, a)
 
 proc genArrayLen(p: BProc, e: CgNode, d: var TLoc, op: TMagic) =
-  var a = e[1]
-  if a.kind == cnkHiddenAddr: a = a.operand
+  let a = e[1]
   var typ = skipTypes(a.typ, abstractVar + tyUserTypeClasses)
   case typ.kind
   of tyOpenArray, tyVarargs:
-    # Bug #9279, len(toOpenArray()) has to work:
-    if getMagic(a) == mSlice:
-      # magic: pass slice to openArray:
-      var b, c: TLoc
-      initLocExpr(p, a[2], b)
-      initLocExpr(p, a[3], c)
-      if op == mHigh:
-        putIntoDest(p, d, e, ropecg(p.module, "($2)-($1)", [rdLoc(b), rdLoc(c)]))
-      else:
-        putIntoDest(p, d, e, ropecg(p.module, "($2)-($1)+1", [rdLoc(b), rdLoc(c)]))
-    else:
+    if true:
       if not reifiedOpenArray(p, a):
         if op == mHigh: unaryExpr(p, e, d, "($1Len_0-1)")
         else: unaryExpr(p, e, d, "$1Len_0")
@@ -1409,6 +1402,8 @@ proc genInOp(p: BProc, e: CgNode, d: var TLoc) =
   if (e[1].kind == cnkSetConstr) and fewCmps(p.config, e[1]):
     # a set constructor but not a constant set:
     # do not emit the set, but generate a bunch of comparisons
+    # XXX: this is currently dead code, but it can be restored once set
+    #      literals are passed to the code generators as constants
     let ea = e[2]
     initLocExpr(p, ea, a)
     initLoc(b, locExpr, e, OnUnknown)
@@ -1674,7 +1669,8 @@ proc binaryFloatArith(p: BProc, e: CgNode, d: var TLoc, m: TMagic) =
     binaryArith(p, e, d, m)
 
 proc skipAddr(n: CgNode): CgNode =
-  result = if n.kind in {cnkAddr, cnkHiddenAddr}: n.operand else: n
+  if n.kind == cnkHiddenAddr: n.operand
+  else:                       n
 
 proc genWasMoved(p: BProc; n: CgNode) =
   var a: TLoc
@@ -1718,7 +1714,8 @@ proc genDestroy(p: BProc; n: CgNode) =
     else: discard "nothing to do"
 
 proc genSlice(p: BProc; e: CgNode; d: var TLoc) =
-  let (x, y) = genOpenArraySlice(p, e, e.typ, e.typ.lastSon)
+  let (x, y) = genOpenArraySlice(p, e, e.typ,
+                                 e.typ.skipTypes(abstractVar).base)
   if d.k == locNone: getTemp(p, e.typ, d)
   linefmt(p, cpsStmts, "$1.Field0 = $2; $1.Field1 = $3;$n", [rdLoc(d), x, y])
   when false:
@@ -1735,6 +1732,7 @@ proc genBreakState(p: BProc, n: CgNode, d: var TLoc) =
 
   let arg = n[1]
   if arg.kind == cnkClosureConstr:
+    # XXX: dead code, but kept as a reminder on what to eventually restore
     initLocExpr(p, arg[1], a)
     r = "(((NI*) $1)[1] < 0)" % [rdLoc(a)]
   else:
@@ -1841,7 +1839,7 @@ proc genMagicExpr(p: BProc, e: CgNode, d: var TLoc, op: TMagic) =
 
     genCall(p, e, d)
   of mDefault: genDefault(p, e, d)
-  of mEcho: genEcho(p, e[1].skipConv)
+  of mEcho: genEcho(p, e)
   of mArrToSeq: genArrToSeq(p, e, d)
   of mNLen..mNError, mStatic..mQuoteAst:
     localReport(p.config, e.info, reportSym(
@@ -1852,7 +1850,7 @@ proc genMagicExpr(p: BProc, e: CgNode, d: var TLoc, op: TMagic) =
       localReport(p.config, e.info, reportSem rsemRequiresDeepCopyEnabled)
 
     var a, b: TLoc
-    let x = if e[1].kind in {cnkAddr, cnkHiddenAddr}: e[1].operand else: e[1]
+    let x = if e[1].kind == cnkHiddenAddr: e[1].operand else: e[1]
     initLocExpr(p, x, a)
     initLocExpr(p, e[2], b)
     genDeepCopy(p, a, b)
@@ -1860,7 +1858,6 @@ proc genMagicExpr(p: BProc, e: CgNode, d: var TLoc, op: TMagic) =
   of mWasMoved: genWasMoved(p, e)
   of mMove: genMove(p, e, d)
   of mDestroy: genDestroy(p, e)
-  of mAccessEnv: unaryExpr(p, e, d, "$1.ClE_0")
   of mAccessTypeField: genAccessTypeField(p, e, d)
   of mSlice: genSlice(p, e, d)
   of mTrace: discard "no code to generate"
@@ -1883,6 +1880,14 @@ proc genMagicExpr(p: BProc, e: CgNode, d: var TLoc, op: TMagic) =
     initLocExpr(p, e[2], b)
     # compare the payloads:
     putIntoDest(p, d, e, "($1.p == $2.p)" % [rdLoc(a), rdLoc(b)])
+  of mCopyInternal:
+    # copy the content of the type field from b to a
+    var a, b: TLoc
+    var check: Rope
+    initLocExpr(p, e[1].operand, a)
+    initLocExpr(p, e[2], b)
+    linefmt(p, cpsStmts, "$1 = $2;$n", [rdMType(p, a, check),
+                                        rdMType(p, b, check)])
   else:
     when defined(debugMagics):
       echo p.prc.name.s, " ", p.prc.id, " ", p.prc.flags, " ", p.prc.ast[genericParamsPos].kind
@@ -1983,19 +1988,9 @@ proc genArrayConstr(p: BProc, n: CgNode, d: var TLoc) =
       arr.r = "$1[$2]" % [rdLoc(d), intLiteral(i)]
       expr(p, n[i], arr)
 
-template genStmtListExprImpl(exprOrStmt) {.dirty.} =
-  #let hasNimFrame = magicsys.getCompilerProc("nimFrame") != nil
-  for i in 0..<n.len - 1:
-    genStmts(p, n[i])
-  if n.len > 0: exprOrStmt
-
-proc genStmtListExpr(p: BProc, n: CgNode, d: var TLoc) =
-  genStmtListExprImpl:
-    expr(p, n[^1], d)
-
 proc genStmtList(p: BProc, n: CgNode) =
-  genStmtListExprImpl:
-    genStmts(p, n[^1])
+  for i in 0..<n.len:
+    genStmts(p, n[i])
 
 proc downConv(p: BProc, n: CgNode, d: var TLoc) =
   ## Generates and emits the code for the ``cnkObjDownConv`` (conversion to
@@ -2187,12 +2182,19 @@ proc expr(p: BProc, n: CgNode, d: var TLoc) =
       let mutate = n.kind == cnkHiddenAddr and n.typ.kind == tyVar
       genAddr(p, n, mutate, d)
   of cnkArrayAccess: genArrayLikeElem(p, n, d)
-  of cnkTupleAccess: genTupleElem(p, n, d)
+  of cnkTupleAccess:
+    if n[0].typ.skipTypes(abstractInst).kind == tyProc:
+      # XXX: temporary workaround. Closures should be normal tuples at this
+      #      stage
+      var a: TLoc
+      initLocExpr(p, n[0], a)
+      putIntoDest(p, d, n, "$1.ClE_0" % [rdLoc(a)])
+    else:
+      genTupleElem(p, n, d)
   of cnkDeref, cnkDerefView: genDeref(p, n, d)
   of cnkFieldAccess: genRecordField(p, n, d)
   of cnkCheckedFieldAccess: genCheckedRecordField(p, n, d)
   of cnkBlockStmt: genBlock(p, n)
-  of cnkStmtListExpr: genStmtListExpr(p, n, d)
   of cnkStmtList: genStmtList(p, n)
   of cnkIfStmt: genIf(p, n)
   of cnkObjDownConv: downConv(p, n, d)
@@ -2221,7 +2223,7 @@ proc expr(p: BProc, n: CgNode, d: var TLoc) =
   of cnkRaiseStmt: genRaiseStmt(p, n)
   of cnkPragmaStmt: discard
   of cnkInvalid, cnkType, cnkAstLit, cnkMagic, cnkRange, cnkBinding, cnkExcept,
-     cnkFinally, cnkBranch, cnkLabel:
+     cnkFinally, cnkBranch, cnkLabel, cnkStmtListExpr:
     internalError(p.config, n.info, "expr(" & $n.kind & "); unknown node kind")
 
 proc getDefaultValue(p: BProc; typ: PType; info: TLineInfo): Rope =

commit 7fa266a4d5594cd8931f8a8439d73ab5135c1abf

mir: fix two `toOpenArray`-related bugs (#1074)
## Summary

Translate the `toOpenArray` magic to a dedicated built-in MIR operator
and fix two bugs related to the magic:
* regression from dc2ff8a4c49f3eb57ce11d84aeb5d33aa46b5f42:
  modifications through a `var openArray` not being visible on the
  source array when the source array is modified as part of the
  `toOpenArray` call's argument expressions
* arrays modified only through `var openArray` sometimes not being
  destroyed, leading to memory leaks (refer to the
  `tconsider_var_openarray` test)

## Details

Calls to the `toOpenArray` magic are represented in the MIR via generic
magic calls, but this doesn't properly capture their behaviour. The
core problem is that the generic magic call doesn't establish a
connection between the array operand and the resulting `openArray`.

Since, at the moment, data-flow analysis doesn't track the roots of
`openArray` values, this also means that creation of a `var openArray`
via `toOpenArray` doesn't count as a mutation of the source value.

In order to resolve these problems, `toOpenArray` is translated to a
dedicated MIR operation.

**Changes**:
* introduce a three-argument variant of the `mnkToSlice` operation (a
  dedicated node kind might be better in the future, but overloading
  the operator is slightly simpler for the moment)
* translate the `mSlice` magic to the three-argument `mnkToSlice`
  variant
* translate the array operand in a way such that the value's identity
  is kept (`genOperand`). This fixes array values possibly being copied
* introduce a dedicated `cnkToSlice` node corresponding to
  `mnkToSlice`. This brings the two IRs closer together
* all three code generators continue to treat the single-argument
  `ToSlice` variant as a no-op for now
* the three-argument variant is treated the same as the `mSlice` magic
  previously was

compiler/backend/ccgcalls.nim
@@ -119,9 +119,9 @@ proc reifiedOpenArray(p: BProc, n: CgNode): bool {.inline.} =
 
 proc genOpenArraySlice(p: BProc; q: CgNode; formalType, destType: PType): (Rope, Rope) =
   var a, b, c: TLoc
-  initLocExpr(p, q[1], a)
-  initLocExpr(p, q[2], b)
-  initLocExpr(p, q[3], c)
+  initLocExpr(p, q[0], a)
+  initLocExpr(p, q[1], b)
+  initLocExpr(p, q[2], c)
   # but first produce the required index checks:
   if optBoundsCheck in p.options:
     genBoundsCheck(p, a, b, c)
@@ -139,7 +139,7 @@ proc genOpenArraySlice(p: BProc; q: CgNode; formalType, destType: PType): (Rope,
         [rdLoc(a), rdLoc(b), intLiteral(first), dest],
         lengthExpr)
   of tyOpenArray, tyVarargs:
-    if reifiedOpenArray(p, q[1]):
+    if reifiedOpenArray(p, q[0]):
       result = ("($3*)($1.Field0)+($2)" % [rdLoc(a), rdLoc(b), dest],
                 lengthExpr)
     else:

compiler/backend/ccgexprs.nim
@@ -1859,7 +1859,6 @@ proc genMagicExpr(p: BProc, e: CgNode, d: var TLoc, op: TMagic) =
   of mMove: genMove(p, e, d)
   of mDestroy: genDestroy(p, e)
   of mAccessTypeField: genAccessTypeField(p, e, d)
-  of mSlice: genSlice(p, e, d)
   of mTrace: discard "no code to generate"
   of mAsgnDynlibVar:
     # initialize the internal pointer for a dynlib global/procedure
@@ -2171,6 +2170,12 @@ proc expr(p: BProc, n: CgNode, d: var TLoc) =
   of cnkObjConstr: genObjConstr(p, n, d)
   of cnkCast: genCast(p, n, d)
   of cnkHiddenConv, cnkConv: genConv(p, n, d)
+  of cnkToSlice:
+    if n.len == 1:
+      # treated as a no-op here; the conversion is handled in ``genAssignment``
+      expr(p, n[0], d)
+    else:
+      genSlice(p, n, d)
   of cnkHiddenAddr, cnkAddr:
     if n.operand.kind in {cnkDerefView, cnkDeref}:
       # views and ``ref``s also map to pointers at the C level. We collapse

compiler/backend/cgir.nim
@@ -84,6 +84,8 @@ type
     # context of code generation. Adjust the code generators so that they no
     # longer depend on ``cnkHiddenConv`` being different from ``cnkConv``, and
     # then remove the former
+    cnkToSlice       ## slice creation. Works the same as the corresponding
+                     ## MIR operation
 
     cnkObjDownConv   ## down conversion between `object` or `ref` types
     cnkObjUpConv     ## up conversion between `object` or `ref` types

compiler/backend/cgirgen.nim
@@ -962,11 +962,8 @@ proc exprToIr(tree: TreeWithSource, cl: var TranslateCl,
 
     adjusted
   of mnkToSlice:
-    # the old code-generators depend on conversions to ``openArray`` to be
-    # omitted
-    let arg = valueToIr(tree, cl, cr)
-    leave(tree, cr)
-    arg
+    treeOp cnkToSlice:
+      res.add valueToIr(tree, cl, cr)
   of mnkAddr:
     op cnkAddr, lvalueToIr(tree, cl, cr)
   of mnkDeref:

compiler/backend/jsgen.nim
@@ -1926,14 +1926,6 @@ proc genMagic(p: PProc, n: CgNode, r: var TCompRes) =
   of mParseBiggestFloat:
     useMagic(p, "nimParseBiggestFloat")
     genCall(p, n, r)
-  of mSlice:
-    # arr.slice([begin[, end]]): 'end' is exclusive
-    var x, y, z: TCompRes
-    gen(p, n[1], x)
-    gen(p, n[2], y)
-    gen(p, n[3], z)
-    r.res = "($1.slice($2, $3 + 1))" % [x.rdLoc, y.rdLoc, z.rdLoc]
-    r.kind = resExpr
   of mMove:
     genMove(p, n, r)
   # of mAccessEnv:
@@ -2397,6 +2389,17 @@ proc gen(p: PProc, n: CgNode, r: var TCompRes) =
   of cnkTupleConstr: genTupleConstr(p, n, r)
   of cnkObjConstr: genObjConstr(p, n, r)
   of cnkHiddenConv, cnkConv: genConv(p, n, r)
+  of cnkToSlice:
+    if n.len == 1:
+      gen(p, n[0], r)
+    else:
+      # arr.slice([begin[, end]]): 'end' is exclusive
+      var x, y, z: TCompRes
+      gen(p, n[0], x)
+      gen(p, n[1], y)
+      gen(p, n[2], z)
+      r.res = "($1.slice($2, $3 + 1))" % [x.rdLoc, y.rdLoc, z.rdLoc]
+      r.kind = resExpr
   of cnkAddr, cnkHiddenAddr:
     if n.typ.kind == tyLent or mapType(n.typ) != etyBaseIndex:
       # the operation doesn't produce an address-like value (e.g. because the

compiler/mir/mirgen.nim
@@ -1017,13 +1017,13 @@ proc genMagic(c: var TCtx, n: PNode; m: TMagic) =
         genx(c, n[1])
         c.userOptions = orig
   of mSlice:
-    c.buildMagicCall mSlice, n.typ:
+    c.buildTree mnkToSlice, n.typ:
       # XXX: a HiddenStdConv erroneously ends up in the array position
       #      sometimes, which would, if kept, lead to index errors when the
       #      array doesn't start at index 0
-      arg skipConv(n[1])
-      arg n[2]
-      arg n[3]
+      genOperand(c, skipConv(n[1]))
+      genArgExpression(c, n[2], sink=false)
+      genArgExpression(c, n[3], sink=false)
 
   # magics that use incomplete symbols (most of them are generated by
   # ``liftdestructors``):

compiler/mir/mirpasses.nim
@@ -128,9 +128,9 @@ proc preventRvo(tree: MirTree, changes: var Changeset) =
              tree[it].typ.skipTypes(abstractVar).kind == tyOpenArray:
             # find the lvalue expression (if any) that the slice was created
             # from and use that for the overlap analysis
-            let def = tree.operand(findDef(tree, NodePosition it), 1)
+            let def = tree.child(findDef(tree, NodePosition it), 1)
             if tree[def].kind == mnkToSlice:
-              (true, tree.operand(def))
+              (true, tree.operand(def, 0))
             else:
               (false, OpValue 0)
           else:

compiler/mir/mirtrees.nim
@@ -141,10 +141,14 @@ type
     # String-to-cstring conversion, and vice versa, should use magics, pointer
     # conversion should use ``mnkCast``
     mnkCast       ## cast the representation of a value into a different type
-    mnkToSlice    ## create an openArray from the full sequence specified as
-                  ## the operand
-    # future direction: also use to ``ToSlice`` for creating sub-slices (i.e.,
-    # ``toOpenArray``)
+    mnkToSlice    ## has to variants:
+                  ## * the 1 argument variant creates an openArray from the
+                  ##   full sequence specified as the operand
+                  ## * the 3 argument variant creates an openArray from the
+                  ##   sub-sequence specified by the sequence and lower and
+                  ##   upper bound
+    # XXX: consider using a separate operator for the slice-from-sub-sequence
+    #      operation
 
     mnkCall   ## invoke a procedure and pass along the provided arguments.
               ## Used for both static and dynamic calls
@@ -305,7 +309,7 @@ const
 
   SingleOperandNodes* = {mnkPathNamed, mnkPathPos, mnkPathVariant, mnkPathConv,
                          mnkAddr, mnkDeref, mnkView, mnkDerefView, mnkStdConv,
-                         mnkConv, mnkCast, mnkToSlice, mnkRaise, mnkTag, mnkArg,
+                         mnkConv, mnkCast, mnkRaise, mnkTag, mnkArg,
                          mnkName, mnkConsume, mnkVoid}
     ## Nodes that start sub-trees but that always have a single sub node.
 

compiler/mir/utils.nim
@@ -231,7 +231,8 @@ proc exprToStr(nodes: MirTree, i: var int, result: var string) =
       valueToStr(nodes, i, result)
   of mnkToSlice:
     tree "toOpenArray ":
-      valueToStr(nodes, i, result)
+      commaSeparated:
+        valueToStr(nodes, i, result)
   of mnkConv:
     tree "conv ":
       valueToStr(nodes, i, result)

compiler/sem/mirexec.nim
@@ -252,13 +252,27 @@ func emitForExpr(env: var ClosureEnv, tree: MirTree, at, source: NodePosition,
     # handles aren't tracked however, the operation is conservatively
     # treated as a mutation
     emitLvalueOp(env, opMutate, tree, at, tree.operand(source))
-  of mnkView, mnkToSlice:
+  of mnkView:
     # if the created view supports mutation, treat the creation as a
     # mutation itself
     let opc =
       if tree[source].typ.kind == tyVar: opMutate
       else:                              opUse
     emitLvalueOp(env, opc, tree, at, tree.operand(source))
+  of mnkToSlice:
+    # slices aren't tracked at the moment, so the mere creation of a slice is
+    # treated as a usage of the sequence. If the resulting openArray supports
+    # mutation, creation of the slice is treated as a mutation. To ensure the
+    # correct data-flow operation order for the mutation case, the lower/upper
+    # bound operands are treated as being evaluated (i.e., used) first
+    if numArgs(tree, source) == 3:
+      emitLvalueOp(env, opUse, tree, at, tree.operand(source, 1))
+      emitLvalueOp(env, opUse, tree, at, tree.operand(source, 2))
+
+    let opc =
+      if tree[source].typ.kind == tyVar: opMutate
+      else:                              opUse
+    emitLvalueOp(env, opc, tree, at, tree.operand(source, 0))
   of LvalueExprKinds:
     # a read or consume is performed on an lvalue
     let opc =

compiler/vm/vmgen.nim
@@ -3006,6 +3006,12 @@ proc gen(c: var TCtx; n: CgNode; dest: var TDest) =
     gen(c, n[0])
   of cnkHiddenConv, cnkConv:
     genConv(c, n, n.operand, dest)
+  of cnkToSlice:
+    if n.len == 1:
+      gen(c, n[0], dest) # treated as a no-op
+    else:
+      # not yet supported
+      fail(n.info, vmGenDiagCodeGenUnhandledMagic, mSlice)
   of cnkObjDownConv, cnkObjUpConv:
     genObjConv(c, n, dest)
   of cnkDef:

tests/lang_objects/destructor/tconsider_var_openarray.nim
@@ -1,0 +1,22 @@
+discard """
+  description: '''
+    Ensure that an openArray created via ``toOpenArray`` and passed to a
+    ``var openArray`` parameter is considered as a mutation, and that the
+    underlying array is treated as alive afterwards
+  '''
+  targets: "c js vm"
+  knownIssue.vm: "`toOpenArray` is not yet supported"
+"""
+
+import mhelper
+
+proc mut(x: var openArray[Resource]) =
+  x[0] = initResource()
+
+proc test() =
+  var arr: array[1, Resource]
+  mut(toOpenArray(arr, 0, 0))
+  # the array must be considered alive and thus destroyed
+
+test()
+doAssert numDestroy == 1

tests/lang_objects/destructor/tv2_cast.nim
@@ -24,17 +24,16 @@ scope:
   try:
     def s: string = newString(arg 100)
     def_cursor _0: string = s
-    def_cursor _1: string = s
-    def_cursor _2: int = len(arg _1)
-    def_cursor _3: int = -(arg _2, arg 1)
-    def_cursor _4: openArray[byte] = slice(arg _0, arg 0, arg _3)
-    def _5: seq[byte] = encode(arg _4) (raises)
+    def_cursor _1: int = len(arg _0)
+    def_cursor _2: int = -(arg _1, arg 1)
+    def_cursor _3: openArray[byte] = toOpenArray s, 0, _2
+    def _4: seq[byte] = encode(arg _3) (raises)
     def data: string
-    def _6: string = cast _5
-    =copy(name data, arg _6)
+    def _5: string = cast _4
+    =copy(name data, arg _5)
   finally:
     =destroy(name data)
-    =destroy(name _5)
+    =destroy(name _4)
     =destroy(name s)
 -- end of expandArc ------------------------
 --expandArc: main2

tests/lang_types/openarray/topenarray.nim
@@ -51,3 +51,22 @@ proc main =
 
 main()
 # static: main() # xxx bug #15952: Error: cannot generate code for: mSlice
+
+block array_modification_not_visible:
+  # modifications of an array through a ``var openArray`` parameter
+  # were not visible on the array if the ``openArray`` parameter was
+  # created via ``toOpenArray`` and the array was *potentially* modified
+  # during evaluation of the lower or upper bound argument expression
+  var arr = [1, 2]
+
+  proc mut(x: var openArray[int]) =
+    x[0] = 3
+
+  # modifying `arr` as part of either the lower or upper bound argument
+  # expression triggered the bug
+  mut(toOpenArray(arr, 0, (arr[0] = 4; 0)))
+  when defined(js):
+    # knownIssue
+    doAssert arr != [3, 2], "works now"
+  else:
+    doAssert arr == [3, 2]