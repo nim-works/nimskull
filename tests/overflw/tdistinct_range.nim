ef86d617b31183d4dcdbf06fab1a517de72fbf5b
zerbina <100542850+zerbina@users.noreply.github.com>
2024-01-13 01:10:50 +0000

    mirgen: insert an additional range check
    This makes the behaviour of `dec`/`inc` exactly the same as that of
    `a = a + b` and `a = a - b`. Neither the JS nor VM code generator
    considered range errors, so this also fixes a long-standing bug...

--- tests/overflw/tdistinct_range.nim
+++ tests/overflw/tdistinct_range.nim
@@ -1,6 +1,10 @@
 discard """
-  outputsub: "Error: unhandled exception: over- or underflow [OverflowDefect]"
+  targets: "c js vm"
+  outputsub: "value out of range: -1 notin 0 .. 5"
   exitcode: "1"
+  knownIssue.js vm: '''
+    correctly raises an exception, but the error message doesn't match
+  '''
 """
 var x: distinct range[0..5]
 dec(x)
