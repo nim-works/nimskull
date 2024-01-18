8234858b350d71e508e744044e9c3bb966f9a6f6
zerbina <100542850+zerbina@users.noreply.github.com>
2024-01-18 23:39:00 +0000

    fix(orc): cycle collection causing heap corruption
    Summary
    =======
    
    In case there were was a reference cycle that was kept alive by a non-
    traced cell, and the non-traced cell was kept alive by a garbage
    reference cycle, running the cycle collector made the collector
    recurse.
    
    A non-traced cell refers to a managed heap cell that's not scanned by
    the cycle collector because its not cyclic (including due to the use
    of `.acyclic`).
    
    Depending on the circumstances, the recursion would either cause a
    stack overflow or a heap corruption. The bug is now fixed.
    
    Details
    =======
    
    The fundamental problem is that a call to `free` can add new roots to
    the global `roots` list, either via automatic destructor or through
    user-defined destructors.
    
    Since neither the `roots` list nor root threshold are reset prior to
    the cleanup phase, a call to `registerCycle` would trigger a
    `collectCycles` recursion, which then ran trial deletion for all
    already scanned roots again.
    
    This would then cause a heap corruption if one of the roots was freed
    already.
    
    The solution
    ------------
    
    Prior to the cleanup phase, all processed roots are removed from the
    `roots` list. In addition, a reentrance guard (`collectorLock`) is
    added that prevents recursion into `collectCycles`/`partialCollect`,
    even if explicitly requested via `GC_fullCollect`.
    
    So that a single full collection frees *all* reference cycles, even
    those only discovered during a run of the cleanup phase,
    `collectCycles` runs the core cycle collection in a loop until no more
    new roots were discovered during cleanup.

---
+++ tests/arc/tcycle_test_acyclic_annotation.nim
@@ -0,0 +1,49 @@
+discard """
+  description: "A variation of ``tcycle_test`` where ``.acyclic`` is used"
+  targets: "c"
+  matrix: "-d:useMalloc"
+  valgrind: true
+"""
+
+type
+  Bridge {.acyclic.} = ref object
+    other: Cycle
+  Cycle = ref object
+    bridge: Bridge
+    self: Cycle
+
+proc init() =
+  # create two cycles and link them together via `Bridge`
+  var a = Cycle(self: Cycle())
+  a.self.self = a
+  var b = Cycle(self: Cycle())
+  b.self.self = b
+
+  b.self.bridge = Bridge(other: a)
+  # the following structure now exists:
+  # A -> B -> Bridge -> C -> D
+  # ^    |              ^    |
+  # + -- +              + -- +
+
+  b = nil # remove the stack root, which marks A as a potential cycle root
+  # 'A' is now the root of a garbage cycle
+
+GC_disableOrc() # disable the cycle collector
+
+var s: seq[Cycle]
+# register enough potential cycle roots for the default collection
+# threshold (128) to overflow
+for i in 0..<1000:
+  var a = Cycle()
+  s.add a
+  # force the `add` to copy by using `a` afterwards. The subsequent destroy
+  # for the ref will add it to the list of potential cycle roots
+  discard a
+
+init() # setup the dangerous cycle
+# first enable the GC again. This is important, as it sets the root list
+# threshold back to its default value
+GC_enableOrc()
+# now force a garbage collection, which results in a recursive cycle collector
+# invocation and thus memory corruption.
+GC_fullCollect()
