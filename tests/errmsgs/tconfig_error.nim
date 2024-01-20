9e564a1e45401878fa5f1ac1bd6dee4dc5b12fb4
alaviss <leorize+oss@disroot.org>
2024-01-20 01:26:25 +0000

    tconfig_error: stylistic fixes
    Co-authored-by: zerbina <100542850+zerbina@users.noreply.github.com>

--- tests/errmsgs/tconfig_error.nim
+++ tests/errmsgs/tconfig_error.nim
@@ -1,9 +1,9 @@
 discard """
   description: '''
-  Test that a config file with invalid option will yield an error with
-  non-zero exit code.
+    Test that a config file with invalid option will yield an error with
+    non-zero exit code.
 
-  Ref https://github.com/nim-works/nimskull/issues/1112
+    References https://github.com/nim-works/nimskull/issues/1112
   '''
   errormsg: "Invalid command line option - totally_valid_config_option"
   file: "tconfig_error.nim.cfg"
