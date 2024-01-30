9c33d31d7702c9d552cfaa3f57c37ae53e61d389
zerbina <100542850+zerbina@users.noreply.github.com>
2024-01-30 00:25:12 +0000

    tests: update `tinvalidpragma`
    An unknown warning name is no longer an error (except if explicitly
    requested).
    
    So far, the test also only tested the well-formed case. It now includes
    tests for `a[b]` (without the on/off parameter) and `a[b, c]` (too many
    bracketed parameters).

--- tests/errmsgs/tinvalidpragma.nim
+++ tests/errmsgs/tinvalidpragma.nim
@@ -2,13 +2,14 @@ discard """
   cmd: "nim check $options $file"
   action: reject
   nimout: '''
-tinvalidpragma.nim(11, 20) Error: invalid pragma: warning[XYZ]: off
-tinvalidpragma.nim(12, 15) Error: invalid pragma: warning[XYZ]: off
-tinvalidpragma.nim(14, 22) Error: invalid pragma: "expression" {.invalid.}
+tinvalidpragma.nim(12, 23) Error: invalid pragma: warning[XYZ, 1]: off
+tinvalidpragma.nim(13, 30) Warning: unknown warning: 'XYZ' [UnknownWarning]
+tinvalidpragma.nim(13, 10) Error: invalid pragma: warning[XYZ]
+tinvalidpragma.nim(15, 22) Error: invalid pragma: "expression" {.invalid.}
 '''
 """
 
-{.push warning[XYZ]: off.}
-{.warning[XYZ]: off, push warning[XYZ]: off.}
+{.push warning[XYZ, 1]: off.}
+{.warning[XYZ], push warning[XYZ]: off.}
 
 discard "expression" {.invalid.}
