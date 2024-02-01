2ad8cbae97a25cffb1c0068135299760e44f4312
zerbina <100542850+zerbina@users.noreply.github.com>
2024-02-01 22:43:17 +0000

    compat: change `translate` to process `MirTree`s
    `translate` is the procedure responsible for translating constant
    expression to the `CgNode` IR. Since constant expressions now use the
    MIR, so has `translate`.

--- compiler/backend/ccgexprs.nim
+++ compiler/backend/ccgexprs.nim
@@ -1963,7 +1963,7 @@ proc genConstDefinition*(q: BModule; id: ConstId) =
   let name = mangleName(q.g.graph, sym)
   if exfNoDecl notin sym.extFlags:
     let p = newProc(nil, q)
-    let data = translate(q.g.env, q.g.env[q.g.env.dataFor(id)])
+    let data = translate(q.g.env[q.g.env.dataFor(id)])
     q.s[cfsData].addf("N_LIB_PRIVATE NIM_CONST $1 $2 = $3;$n",
         [getTypeDesc(q, sym.typ), name,
         genBracedInit(p, data, sym.typ)])
@@ -1983,7 +1983,7 @@ proc useData(p: BProc, x: ConstId, typ: PType): string =
     inc p.module.labels
     p.module.s[cfsData].addf("static NIM_CONST $1 $2 = $3;$n",
       [getTypeDesc(p.module, typ), result,
-       genBracedInit(p, translate(p.env, p.env[id]), typ)])
+       genBracedInit(p, translate(p.env[id]), typ)])
 
 proc expr(p: BProc, n: CgNode, d: var TLoc) =
   when defined(nimCompilerStacktraceHints):
@@ -2003,7 +2003,7 @@ proc expr(p: BProc, n: CgNode, d: var TLoc) =
     if isSimpleConst(p.config, n.typ):
       # simple constants are inlined at the usage site
       let da = p.env.dataFor(n.cnst)
-      let val = translate(p.env, p.env[da])
+      let val = translate(p.env[da])
       if val.kind == cnkSetConstr:
         let cs = toBitSet(p.config, val)
         putIntoDest(p, d, n, genRawSetData(cs, int(getSize(p.config, n.typ))))
