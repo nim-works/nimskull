d33acc5283d79d5d1b2bb1660c35da33b8e00c99
zerbina <100542850+zerbina@users.noreply.github.com>
2024-02-04 18:13:54 +0000

    tests: add tests for the fixed issues

---
+++ tests/exception/traise_and_handle_in_except.nim
@@ -0,0 +1,31 @@
+discard """
+  description: '''
+    Ensure that raising and fully handling an exception within an ``except``
+    branch works.
+  '''
+  output: "done"
+  knownIssue.js: "The current exception is not properly cleared"
+"""
+
+var steps: seq[int]
+
+try:
+  raise ValueError.newException("a")
+except ValueError as e:
+  steps.add 1
+  try:
+    raise ValueError.newException("b")
+  except ValueError as e:
+    steps.add 2
+    doAssert e.msg == "b"
+    doAssert getCurrentException() == e
+
+  steps.add 3
+  # make sure the current exception is still the correct one
+  doAssert getCurrentException() == e
+
+steps.add 4
+doAssert getCurrentException() == nil, "current exception wasn't cleared"
+doAssert steps == [1, 2, 3, 4]
+
+echo "done" # make sure the assertions weren't skipped over
