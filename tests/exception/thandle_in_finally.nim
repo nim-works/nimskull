6e9f7ec8dd525c634f9763137951ce7ddeb7b351
zerbina <100542850+zerbina@users.noreply.github.com>
2024-02-04 18:13:54 +0000

    tests: add a test for the "reraise-and-handle-in-finally" case
    It doesn't work at the moment, but it's also not entirely clear whether
    it actually should.

---
+++ tests/exception/thandle_in_finally.nim
@@ -0,0 +1,33 @@
+discard """
+  description: '''
+    Ensure that a finally clause can fully handle an exception by re-raising
+    and catching it
+  '''
+  knownIssue.c js vm: '''
+    Not implemented properly by the code generator and/or runtime
+  '''
+"""
+
+# XXX: it's questionable whether this really should work / be allowed
+
+var steps: seq[int]
+
+try:
+  try:
+    raise CatchableError.newException("a")
+  finally:
+    try:
+      raise # re-raise the active exception
+    except CatchableError:
+      # catch the exception
+      steps.add 1
+      # leaving this except handler means that the exception is handled
+    steps.add 2
+    doAssert getCurrentException() == nil
+    # unwinding doesn't continue when leaving the finally
+  steps.add 3
+except CatchableError:
+  # never reached, since the excepti
+  doAssert false, "unreachable"
+
+doAssert steps == [1, 2]
