7a0a245fc3d2606d891d254d78a179f429b13f4d
zerbina <100542850+zerbina@users.noreply.github.com>
2024-02-05 01:05:21 +0000

    tests: update the `thandle_in_finally` test
    * reword the description to better highlight the uncertainty
    * change the test's success condition to all three steps
    * fix the test running forever due the signal handler being entered
      over and over

--- tests/exception/thandle_in_finally.nim
+++ tests/exception/thandle_in_finally.nim
@@ -1,14 +1,15 @@
 discard """
   description: '''
-    Ensure that a finally clause can fully handle an exception by re-raising
-    and catching it
+    A test for tracking the (uncertain) behaviour of entering a `finally`
+    clause through an exception, handling the exception within the
+    `finally`, and then leaving through structured control-flow.
   '''
-  knownIssue.c js vm: '''
-    Not implemented properly by the code generator and/or runtime
-  '''
+  matrix: "-d:noSignalHandler"
+  knownIssue: "true"
 """
 
-# XXX: it's questionable whether this really should work / be allowed
+# XXX: disabling the signal handler is currently necessary for the test
+#      to crash rather than entering an infinite loop
 
 var steps: seq[int]
 
@@ -24,10 +25,11 @@ try:
       # leaving this except handler means that the exception is handled
     steps.add 2
     doAssert getCurrentException() == nil
-    # unwinding doesn't continue when leaving the finally
+    # unwinding cannot continue when leaving the finally, since no exception
+    # is active anymore
   steps.add 3
 except CatchableError:
   # never reached, since the exception was handled above
   doAssert false, "unreachable"
 
-doAssert steps == [1, 2]
+doAssert steps == [1, 2, 3]
