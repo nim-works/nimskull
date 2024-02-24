3416e8e7d362841979b01c27e2bf738a54ff0ade
Saem Ghani <saemghani+github@gmail.com>
2024-02-24 12:35:53 -0800

    varargs argument parsing is entirely broken
    until it's fixed going to be marking lots of tests as known issue

--- tests/lang_callable/varargs/tvarargs_non_ast_types_conversion.nim
+++ tests/lang_callable/varargs/tvarargs_non_ast_types_conversion.nim
@@ -2,6 +2,7 @@ discard """
   description: '''
     Tests for non-ast varargs, those that are not `un/typed`, with conversions.
   '''
+  knownIssue: "argument handling is just plain broken"
 """
 
 block conversions_for_non_ast_varargs:
