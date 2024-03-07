5f4c2fe9cb4075b0cf458760ee187d7e43b52e8b
zerbina <100542850+zerbina@users.noreply.github.com>
2024-03-07 18:41:50 +0000

    tests: enable now-working tests
    'leave' EH instructions were previously not emitted properly, leading
    to the EH threads not being quit properly and the current exception
    thus being wrong.

--- tests/exception/treraise2.nim
+++ tests/exception/treraise2.nim
@@ -3,7 +3,7 @@ discard """
     Ensure that raising a caught exception from within an exception handler
     works
   '''
-  knownIssue.js vm: "The current exception is not properly updated"
+  knownIssue.js: "The current exception is not properly updated"
 """
 
 proc manualReraise() =
