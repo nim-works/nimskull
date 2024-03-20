9d5714bf5cedc5b2426ecfee7a6389615a60db97
zerbina <100542850+zerbina@users.noreply.github.com>
2024-03-20 21:25:21 +0000

    spec: rename two tests
    The tests are about custom *instance* types, not *coroutine* types.

--- tests/lang/s02_core/s99_coroutines/t15_custom_coroutine_instance_type.nim
+++ tests/lang/s02_core/s99_coroutines/t15_custom_coroutine_instance_type.nim
@@ -3,7 +3,7 @@ discard """
 """
 
 ## The ``.coroutine`` pragma can be supplied with a type, which the internal
-## coroutine environment object then derives from. The type must be a non-
+## coroutine instance object then derives from. The type must be a non-
 ## final ``ref`` sub-type of ``Coroutine[T]``, where `T` is the return type
 ## of the coroutine.
 
@@ -16,7 +16,7 @@ proc coro() {.coroutine: Custom.} =
   # the hidden `self` parameter is also of type ``Custom``
   echo self.value
 
-## The instance returned by ``launch`` is of the provided coroutine type.
+## The instance returned by ``launch`` is of the provided type.
 var instance = launch coro()
 doAssert typeof(instance) is Custom
 doAssert instance.value == 0
