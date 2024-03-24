74f3796e0094540871eb73ae7e3d54ffa26dcb61
zerbina <100542850+zerbina@users.noreply.github.com>
2024-03-24 00:56:58 +0000

    spec: small wording improvement

--- tests/lang/s02_core/s99_coroutines/t05_resume_coroutine.nim
+++ tests/lang/s02_core/s99_coroutines/t05_resume_coroutine.nim
@@ -3,10 +3,10 @@ discard """
 """
 
 ## To resume a coroutine instance (that is, run it until the next suspension
-## point is reached), the built-in nullary ``resume`` routine is used. The
-## ``resume`` procedure returns a coroutine instance, which, by default, is
-## the same one that was resumed. Only an instance that is in the "suspended"
-## state can be resumed.
+## point is reached), the built-in nullary ``resume`` procedure is used. The
+## ``resume`` procedure returns the coroutine instance that the coroutine
+## suspended with. Only an instance that is in the "suspended" state can be
+## resumed.
 ##
 ## Before passing control to the coroutine, ``resume`` sets the instance's
 ## status to "running"
@@ -19,4 +19,4 @@ discard resume(instance) # the echo will be executed
 
 ## When a suspension point is reached, ``resume`` returns. If there's no more
 ## code to run within the coroutine, prior to returning from ``resume``, the
-## state is set to "pending", otherwise it's set to "suspendend".
+## status is set to "pending", otherwise it's set to "suspendend".
