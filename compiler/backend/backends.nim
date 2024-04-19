073531ebc053cf7036c7b88689f3461b9860c2cc
zerbina <100542850+zerbina@users.noreply.github.com>
2024-04-19 22:39:50 +0000

    backends: remove `mnkStmtList` usage
    The node kind is gone.

--- compiler/backend/backends.nim
+++ compiler/backend/backends.nim
@@ -263,7 +263,7 @@ func isEmpty*(tree: MirTree): bool =
   ## Returns whether `tree` contains either no nodes or only nodes that have
   ## no meaning by themselves.
   for n in tree.items:
-    if n.kind notin {mnkScope, mnkStmtList, mnkEnd}:
+    if n.kind notin {mnkScope, mnkEnd}:
       return false
 
   result = true
