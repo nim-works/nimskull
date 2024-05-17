nim-works/nimskull

commit ae543125c222693c915e3cfaa3cc3edb0110c569

js: support variant destructors (#1267)
## Summary

Fix destructors not being properly executed for fields within case
objects when changing the active branch, and using the JS backend.

## Details

The destructor produced by `produceDestructorForDiscriminator` took a
reference to the *discriminant field* and used pointer arithmetic to
compute the address of the enclosing object. Not only is this
unnecessarily complex, it also doesn't work with the JS backend.

Instead, the destructor now takes a mutable reference to the
discriminant field's enclosing object type -- the MIR emitted for the
call is adjusted accordingly.

Since the destructor definitely modifies the object, `ekMutate` is used
instead of `ekInvalidate` for the parameter passing. This results in a
small move analyzer regression:
```nim
var x = Object()
var y = x.fieldOutsideOfVariant # not automatically moved anymore
x.kind = ... # potential branch switch that requires destruction
```

Finally, a test is added for ensuring proper destruction when changing
the active branch.

compiler/sem/injectdestructors.nim
@@ -813,14 +813,19 @@ proc lowerBranchSwitch(bu: var MirBuilder, body: MirTree, graph: ModuleGraph,
       bu.buildMagicCall mNot, boolTyp:
          bu.emitByVal val
 
+    var src = body.child(NodePosition target, 0)
+    # skip all ``mnkPathVariant`` nodes:
+    while body[src].kind == mnkPathVariant:
+      src = body.child(src, 0)
+
     bu.subTree mnkIf:
       bu.use val
       # ``=destroy`` call:
       bu.buildVoidCall(env, branchDestructor):
-        # pass the original variant access to the destroy call
+        # pass the object access expression to the destroy call
         bu.subTree mnkName:
-          bu.subTree MirNode(kind: mnkTag, effect: ekInvalidate):
-            bu.emitFrom(body, NodePosition target)
+          bu.subTree MirNode(kind: mnkTag, effect: ekMutate):
+            bu.emitFrom(body, src)
 
   else:
     # the object doesn't need destruction, which means that neither does one

compiler/sem/liftdestructors.nim
@@ -125,29 +125,6 @@ proc genWhileLoop(c: var TLiftCtx; i, dest: PNode): PNode =
 proc genIf(c: var TLiftCtx; cond, action: PNode): PNode =
   result = newTree(nkIfStmt, newTree(nkElifBranch, cond, action))
 
-proc genContainerOf(c: var TLiftCtx; objType: PType, field, x: PSym): PNode =
-  # generate: cast[ptr ObjType](cast[int](addr(x)) - offsetOf(objType.field))
-  let intType = getSysType(c.g, unknownLineInfo, tyInt)
-
-  let addrOf = newTreeIT(nkAddr, c.info, makePtrType(x.owner, x.typ, c.idgen)):
-    newDeref(newSymNode(x))
-  let castExpr1 = newTreeIT(nkCast, c.info, intType):
-    [newNodeIT(nkType, c.info, intType), addrOf]
-
-  let dotExpr = newTreeIT(nkDotExpr, c.info, x.typ):
-    [newNodeIT(nkType, c.info, objType), newSymNode(field)]
-
-  let offsetOf = genBuiltin(c, mOffsetOf, "offsetof", dotExpr)
-  offsetOf.typ = intType
-
-  let minusExpr = genBuiltin(c, mSubI, "-", castExpr1)
-  minusExpr.typ = intType
-  minusExpr.add foldOffsetOf(c.g.config, offsetOf, offsetOf)
-
-  let objPtr = makePtrType(objType.owner, objType, c.idgen)
-  result = newTreeIT(nkCast, c.info, objPtr):
-    [newNodeIT(nkType, c.info, objPtr), minusExpr]
-
 proc destructorCall(c: var TLiftCtx; op: PSym; x: PNode): PNode =
   var destroy = newTreeIT(nkCall, x.info, op.typ[0]):
     [newSymNode(op), genAddr(c, x)]
@@ -948,23 +925,16 @@ proc produceSym(g: ModuleGraph; c: PContext; typ: PType; kind: TTypeAttachedOp;
 proc produceDestructorForDiscriminator*(g: ModuleGraph; typ: PType; field: PSym,
                                         info: TLineInfo; idgen: IdGenerator): PSym =
   assert(typ.skipTypes({tyAlias, tyGenericInst}).kind == tyObject)
-  result = symPrototype(g, field.typ, typ.owner, attachedDestructor, info, idgen)
+  result = symPrototype(g, typ, typ.owner, attachedDestructor, info, idgen)
   var a = TLiftCtx(info: info, g: g, kind: attachedDestructor, asgnForType: typ, idgen: idgen,
                    fn: result)
   a.asgnForType = typ
   a.filterDiscriminator = field
   a.addMemReset = true
-  let discrimantDest = result.typ.n[1].sym
-
-  let dst = newSym(skVar, getIdent(g.cache, "dest"), nextSymId(idgen), result, info)
-  dst.typ = makePtrType(typ.owner, typ, idgen)
-  let dstSym = newSymNode(dst)
-  let d = newDeref(dstSym)
-  let v = newTreeI(nkVarSection, info):
-    newIdentDefs(dstSym, genContainerOf(a, typ, field, discrimantDest))
-  result.ast[bodyPos].add v
-  let placeHolder = newNodeIT(nkSym, info, getSysType(g, info, tyPointer))
-  fillBody(a, typ, result.ast[bodyPos], d, placeHolder)
+  let
+    d = newDeref(newSymNode(result.typ.n[1].sym))
+    placeholder = newNodeIT(nkSym, info, getSysType(g, info, tyPointer))
+  fillBody(a, typ, result.ast[bodyPos], d, placeholder)
   incl result.flags, sfNeverRaises
 
 

tests/lang_objects/destructor/tcaseobj_transition_2.nim
@@ -1,0 +1,39 @@
+discard """
+  description: '''
+    Ensure that the destructor of a field within a case obj is triggered when
+    changing the active case object branch
+  '''
+  targets: "c js vm"
+  knownIssue.vm: '''
+    Lowering branch switch statements is disabled for the VM backend
+  '''
+"""
+
+type
+  Object = object
+    name: int
+
+var steps: seq[int]
+
+proc `=destroy`(x: var Object) =
+  steps.add x.name
+
+type
+  WithVariant = object
+    outer: Object
+      ## field not within the case object; unaffected by branch changing
+    case kind: bool
+    of false:
+      a: Object
+    of true:
+      b: Object
+
+proc test() =
+  var o = WithVariant(outer: Object(name: 1), kind: false, a: Object(name: 2))
+  o.kind = true # change branch; `a` is destroyed
+  doAssert o.b.name == 0 # uninitialized
+  o.b.name = 3
+  # on destruction, both `b` and `outer` must be destroyed
+
+test()
+doAssert steps == [2, 1, 3], $steps

commit a6d4723ea9590be11ab99c03b34063d2ef2ceed7

fix: move analyzer regression with variants (#1311)
## Summary

Separate the `switch` lowering and destructor injection into two proper
passes and apply them concurrently, fixing a move analyzer regression.

## Details

* move the `switch` lowering out of `injectDestructorCalls`
* pass a `Changeset` instead of a mutable `MirBody` to the two passes
  in `injectdestructors`
* destructor injection and `switch` lowering are applied in a single
  batch by `backends.process` 
* the `switch` lowering is still disabled for the VM backend
* update the doc comment of `injectDestructorCalls` to reflect reality

### Move Analyzer

Applying both passes concurrently - instead of the destructor-
injection/move-analysis after `switch` lowering - fixes a regression
from ae543125c222693c915e3cfaa3cc3edb0110c569

The move analyzer now sees a discriminator assignment as:
```nim
x.discr = y
```
instead of:
```nim
if x.discr != y:
  =destroy(x) # variant destructor
x.discr = y
```

allowing it to perform automatic moves from non-variant fields again
(refer to `tmove_from_non_variant_field.nim`).

### Misc

* fix MIR pretty-printing for object/ref constructors -- the colon was
  missing between the field and value

compiler/backend/backends.nim
@@ -26,6 +26,7 @@ import
     injecthooks,
     mirbodies,
     mirbridge,
+    mirchangesets,
     mirconstr,
     mirenv,
     mirgen,
@@ -347,7 +348,18 @@ proc process(body: var MirBody, prc: PSym, graph: ModuleGraph,
   ## Applies all applicable MIR passes to the `body`. `prc` is the enclosing
   ## procedure.
   if shouldInjectDestructorCalls(prc):
-    injectDestructorCalls(graph, idgen, env, prc, body)
+    block:
+      var c = initChangeset(body)
+      injectDestructorCalls(body.code, graph, env, c)
+      # XXX: ``vmgen`` doesn't support the code resulting from the switch
+      #      lowering, so branch destructors are disabled for the VM target
+      #      at the moment
+      if graph.config.backend != backendNimVm:
+        lowerBranchSwitch(body.code, graph, idgen, env, c)
+      body.apply(c)
+
+    # hook injection needs to happen *after* move analysis and destroy
+    # injection
     injectHooks(body, graph, env, prc)
 
     if graph.config.arcToExpand.hasKey(prc.name.s):

compiler/mir/utils.nim
@@ -418,6 +418,7 @@ proc exprToStr(nodes: MirTree, i: var int, result: var string, c: RenderCtx) =
     tree "(":
       commaSeparated:
         fieldToStr(next(nodes, i).field, typ, result, c)
+        result.add ": "
         argToStr()
       result.add ")"
   of mnkCall:

compiler/sem/injectdestructors.nim
@@ -826,56 +826,35 @@ func shouldInjectDestructorCalls*(owner: PSym): bool =
      {sfInjectDestructors, sfGeneratedOp} * owner.flags == {sfInjectDestructors} and
      (owner.kind != skIterator or not isInlineIterator(owner.typ))
 
-proc injectDestructorCalls*(g: ModuleGraph, idgen: IdGenerator,
-                            env: var MirEnv, owner: PSym,
-                            body: var MirBody) =
-  ## The ``injectdestructors`` pass entry point. The pass is made up of
-  ## multiple sub-passes, hence the mutable `body` (as opposed
-  ## to returning a ``Changeset``).
-  ##
-  ## For now, semantic errors and other diagnostics related to lifetime-hook
-  ## usage are also reported here.
+proc lowerBranchSwitch*(tree: MirTree, g: ModuleGraph, idgen: IdGenerator,
+                        env: var MirEnv, changes: var Changeset) =
+  ## Lowers ``mnkSwitch`` operations into normal assignments, with a branch
+  ## destructor injected if the respective record-case requires it (i.e.,
+  ## because it contains fields requiring destruction).
+  for i, n in tree.pairs:
+    if n.kind == mnkSwitch:
+      changes.replaceMulti(tree, i, buf):
+        lowerBranchSwitch(buf, tree, g, idgen, env, i)
 
-  # apply the first batch of passes:
-  block:
-    var changes = initChangeset(body)
-    # the VM implements branch switching itself - performing the lowering for
-    # code meant to run in it would be harmful
-    # FIXME: discriminant assignment lowering also needs to be disabled for
-    #        when generating code running at compile-time (e.g. inside a
-    #        macro)
-    # XXX: the lowering is *always* necessary, as the destructors for
-    #      fields inside switched-away-from branches won't be called
-    #      otherwise
-    # TODO: make the branch-switch lowering a separate and standalone pass --
-    #       it's not directly related to the rest of the processing here
-    if g.config.backend != backendNimVm:
-      for i, n in body.code.pairs:
-        if n.kind == mnkSwitch:
-          changes.replaceMulti(body.code, i, buf):
-            lowerBranchSwitch(buf, body.code, g, idgen, env, i)
-
-    body.apply(changes)
-
-  # apply the second batch of passes:
+proc injectDestructorCalls*(tree: MirTree, g: ModuleGraph, env: var MirEnv,
+                            changes: var Changeset) =
+  ## Collapses sink assignments into either copy or move assignments, and
+  ## injects the destroy operations for all entities requiring destruction.
   block:
     var
-      changes = initChangeset(body)
-      actx = AnalyseCtx(graph: g, cfg: computeDfg(body.code))
+      actx = AnalyseCtx(graph: g, cfg: computeDfg(tree))
 
     let
-      entities = initEntityDict(body.code, actx.cfg, env)
-      moves = collapseSink(body.code, actx.cfg, entities, env.types)
+      entities = initEntityDict(tree, actx.cfg, env)
+      moves = collapseSink(tree, actx.cfg, entities, env.types)
 
-    let destructors = computeDestructors(body.code, actx.cfg, entities)
+    let destructors = computeDestructors(tree, actx.cfg, entities)
 
     rewriteAssignments(
-      body.code, actx,
+      tree, actx,
       AnalysisResults(moves: cursor(moves),
                       entities: cursor(entities),
                       destroy: cursor(destructors)),
       env.types, changes)
 
-    injectDestructors(body.code, g, destructors, env, changes)
-
-    body.apply(changes)
+    injectDestructors(tree, g, destructors, env, changes)

tests/optimization/tmove_from_non_variant_field.nim
@@ -1,0 +1,43 @@
+discard """
+  description: '''
+    Ensure that a discriminator assignment doesn't prevent automatic moves
+    from fields outside the record-case
+  '''
+  action: compile
+  matrix: "--expandArc:test --checks:off"
+  nimout: '''--expandArc: test
+scope:
+  def _3: WithHooks = ()
+  def x: Object = (a: consume _3, kind: consume true)
+  bind_mut _4: WithHooks = x.a
+  result := move _4
+  wasMoved(name _4)
+  bind_mut _5: bool = x.kind
+  def _6: bool = copy kind
+  def _7: bool = eqB(arg _5, arg _6)
+  def _8: bool = not(arg _7)
+  if _8:
+    =destroy(name x)
+  _5 = _6
+  =destroy(name x)
+
+-- end of expandArc ------------------------'''
+"""
+
+type
+  WithHooks = object
+  Object = object
+    a: WithHooks
+    case kind: bool
+    of true, false:
+      b: WithHooks
+
+proc `=destroy`(x: var WithHooks) =
+  discard
+
+proc test(kind: bool): WithHooks {.exportc.} =
+  var x = Object(a: WithHooks(), kind: true)
+  result = x.a # can be moved
+  # the discriminator assignment must not prevent `x.a` from being moved out
+  # of
+  x.kind = kind