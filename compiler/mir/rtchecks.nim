nim-works/nimskull

commit 29dc0c775da9e2a96e022f39c037a6db15cf415d

lower `mChckIndex` with MIR pass (#1324)
## Summary

Lower `mChckIndex` magic calls with an MIR pass, instead of as part of
C code generation. This is a pure refactoring, with the goal of
shrinking down the C code generator.

## Details

The lowering is integrated into `rtchecks`, and is a MIR port of the
lowering previously performed by `cgen.genIndexCheck` (which is now
removed), including two pre-existing bugs. The two bugs are documented
in the code, and a test (`tout_of_range_index_check.nim`) is added for
one them.

The lowering needs to query the unsigned version of the "size type", so
the `usizeType` query is added for `TypeEnv`.

Finally, a leftover `unreachable` debug statement that triggers on
unused labels is removed from `mirexec`: it would trigger when an index
check on a `cstring` (which gets eliminated by the lowering) is the
only jump-source for a label.

compiler/mir/rtchecks.nim
@@ -75,6 +75,9 @@ template emitCall(bu; tree; call; prc: ProcedureId, arguments: untyped) =
 proc addCompilerProc(env; graph; name: string): ProcedureId =
   env.procedures.add(graph.getCompilerProc(name))
 
+proc makeLiteral(env; kind: MirNodeKind, val: Int128, typ: TypeId): Value =
+  literal(kind, env.getOrIncl(val.toUInt.BiggestUInt), typ)
+
 proc getInt(env: MirEnv, n: MirNode): Int128 =
   case n.kind
   of mnkIntLit:  toInt128 env.getInt(n.number)
@@ -200,6 +203,107 @@ proc emitNanCheck(tree; call; graph; env; bu) =
   bu.buildIfNot cmp:
     bu.emitCall(tree, call, env.addCompilerProc(graph, "raiseFloatInvalidOp")):
       discard
+
+proc emitIndexCheck(tree; call; graph; env; bu) =
+  ## Emits the lowered index check for `call`.
+  let
+    arrOperand = NodePosition tree.argument(call, 0)
+    ty = env[tree[arrOperand].typ].skipTypes(abstractInst +
+      tyUserTypeClasses + {tyLent, tyVar})
+
+  if ty.kind == tyCstring:
+    # no index check needed
+    # XXX: index checks should be omitted for cstrings when using the C
+    #      backend
+    return
+
+  let
+    idxOperand = NodePosition tree.argument(call, 1)
+    sizeType  = env.types.sizeType
+    usizeType = env.types.usizeType
+
+  if ty.kind == tyArray and (firstOrd(graph.config, ty) != Zero or
+     lastOrd(graph.config, ty) < Zero):
+    # FIXME: this branch also need to be taken when lastOrd >= high(int)
+    # we need to test against both the lower and upper bound
+    let
+      typ   = env.types.add(env[tree[arrOperand].typ][0])
+      first = env.makeLiteral(mnkIntLit, firstOrd(graph.config, ty), typ)
+      last  = env.makeLiteral(mnkIntLit, lastOrd(graph.config, ty), typ)
+
+    # FIXME: there are two problems here:
+    #        * the comparison operator is wrong for non-int types
+    #        * the comparison operands don't use the same type
+    #        As a consequence, C integer promotion rules apply, leading to
+    #        incorrect test results in some cases. The bound values need to
+    #        be converted to the index operand's type first, with boundary
+    #        checks omitted where the boundary's value cannot be represented
+    #        with the index operand's type
+
+    let cond = bu.wrapTemp BoolType:
+      bu.buildMagicCall mLtI, BoolType:
+        bu.subTree mnkArg:
+          bu.emitFrom(tree, idxOperand)
+        bu.emitByVal first
+
+    bu.buildIfNot cond:
+      bu.subTree mnkAsgn:
+        bu.use cond
+        bu.buildMagicCall mLtI, BoolType:
+          bu.emitByVal last
+          bu.subTree mnkArg:
+            bu.emitFrom(tree, idxOperand)
+
+    bu.buildIf cond:
+      bu.emitCall(tree, call, env.addCompilerProc(graph, "raiseIndexError3")):
+        bu.subTree mnkArg:
+          bu.emitFrom(tree, idxOperand)
+        bu.emitByVal first
+        bu.emitByVal last
+
+  else:
+    # if the first index is at 0 and the length is guaranteed to be in range
+    # 0..high(int), an optimization is used: the operand is coverted to a
+    # first, and then only a single comparison against the length is used.
+    # This works because a negative index operand becomes a value > high(int)
+    # after to-uint conversion
+    var len: Value
+    if ty.kind == tyArray:
+      # the length is static
+      len = env.makeLiteral(mnkUIntLit, lengthOrd(graph.config, ty), usizeType)
+    else:
+      # the length is dynamic
+      let tmp = bu.wrapTemp sizeType:
+        bu.buildMagicCall mLengthOpenArray, sizeType:
+          bu.subTree mnkArg:
+            bu.emitFrom(tree, arrOperand)
+
+      # convert the length:
+      len = bu.wrapTemp usizeType:
+        bu.subTree mnkConv, usizeType:
+          bu.use tmp
+
+    # convert the index:
+    let idx = bu.wrapTemp usizeType:
+      bu.subTree mnkConv, usizeType:
+        bu.emitFrom(tree, idxOperand)
+
+    # compare the values:
+    let cond = bu.wrapTemp BoolType:
+      bu.buildMagicCall mLeU, BoolType:
+        bu.emitByVal len
+        bu.emitByVal idx
+
+    bu.buildIf cond:
+      let val = bu.wrapTemp usizeType:
+        bu.subTree mnkSub, usizeType:
+          bu.use len
+          bu.use literal(mnkUIntLit, env.getOrIncl(1), usizeType)
+
+      bu.emitCall(tree, call, env.addCompilerProc(graph, "raiseIndexError2")):
+        bu.subTree mnkArg:
+          bu.emitFrom(tree, idxOperand)
+        bu.emitByVal val
 
 proc emitFieldCheck(tree; source: SourceMap; call; graph; env; bu) =
   ## For ``chckField(set, discr, invert, msg)`` emits the MIR equivalent of:
@@ -346,6 +450,10 @@ proc lowerChecks*(body; graph; env; changes: var Changeset) =
         # make sure to take the ``mnkVoid`` wrapper into account
         changes.replaceMulti(tree, tree.parent(call), bu):
           emitNanCheck(tree, call, graph, env, bu)
+      of mChckIndex:
+        let call = tree.parent(i)
+        changes.replaceMulti(tree, tree.parent(call), bu):
+          emitIndexCheck(tree, call, graph, env, bu)
       of mChckField:
         let call = tree.parent(i)
         # make sure to take the ``mnkVoid`` wrapper into account

tests/lang_types/array/tout_of_range_index_check.nim
@@ -1,0 +1,29 @@
+discard """
+  description: '''
+    Ensure that accessing an array works when the index operand's type cannot
+    be safely converted to the array's index type
+  '''
+  knownIssue.c: '''
+    The boundary checks are implemented improperly, leading to the array
+    appearing to effectively be empty
+  '''
+  knownIssue.vm: '''
+    Arrays with a start index outside of -128..127 crash the code generator
+  '''
+"""
+
+proc test1(index: int): int =
+  # try with an index range that overlaps with the `int` range
+  var arr: array[uint(high(int))..(uint(high(int)) + 2), int] = [1, 2, 3]
+  # not all valid array indices can be represented by `int`
+  result = arr[index]
+
+# the index is valid, no index error must be raised
+doAssert test1(high(int)) == 1
+
+proc test2(index: uint): int =
+  var arr: array[-1..1, int] = [1, 2, 3]
+  # not all valid array indices can be represented by `uint`
+  result = arr[index]
+
+doAssert test2(1) == 3

compiler/backend/ccgexprs.nim
@@ -620,38 +620,6 @@ proc genBoundsCheck(p: BProc; arr, a, b: TLoc, exit: CgNode) =
   else:
     unreachable(ty.kind)
 
-proc genIndexCheck(p: BProc; x: CgNode, arr, idx: TLoc, exit: CgNode) =
-  ## Emits the index check logic + subsequent raise operation. `x` is
-  ## the array expression the `arr` loc resulted from from.
-  let ty = arr.t.skipTypes(abstractVar + tyUserTypeClasses +
-                           {tyPtr, tyRef, tyLent, tyVar})
-  case ty.kind
-  of tyArray:
-    var first = intLiteral(firstOrd(p.config, ty))
-    if firstOrd(p.config, ty) == 0 and lastOrd(p.config, ty) >= 0:
-      linefmt(p, cpsStmts, "if ((NU)($1) > (NU)($2)){ #raiseIndexError2($1, $2); $3}$n",
-              [rdCharLoc(idx), intLiteral(lastOrd(p.config, ty)),
-               raiseInstr(p, exit)])
-    else:
-      linefmt(p, cpsStmts, "if ($1 < $2 || $1 > $3){ #raiseIndexError3($1, $2, $3); $4}$n",
-              [rdCharLoc(idx), first, intLiteral(lastOrd(p.config, ty)),
-               raiseInstr(p, exit)])
-  of tySequence, tyString:
-    linefmt(p, cpsStmts,
-            "if ((NU)($1) >= (NU)$2){ #raiseIndexError2($1,$2-1); $3}$n",
-            [rdCharLoc(idx), lenExpr(p, arr), raiseInstr(p, exit)])
-  of tyOpenArray, tyVarargs:
-    if reifiedOpenArray(p, x):
-      linefmt(p, cpsStmts, "if ((NU)($1) >= (NU)($2.Field1)){ #raiseIndexError2($1,$2.Field1-1); $3}$n",
-              [rdCharLoc(idx), rdLoc(arr), raiseInstr(p, exit)])
-    else:
-      linefmt(p, cpsStmts, "if ((NU)($1) >= (NU)($2Len_0)){ #raiseIndexError2($1,$2Len_0-1); $3}$n",
-              [rdCharLoc(idx), rdLoc(arr), raiseInstr(p, exit)])
-  of tyCstring:
-    discard "no bound checks"
-  else:
-    unreachable()
-
 proc genOpenArrayElem(p: BProc, n, x, y: CgNode, d: var TLoc) =
   var a, b: TLoc
   initLocExpr(p, x, a)
@@ -1529,11 +1497,6 @@ proc genMagicExpr(p: BProc, e: CgNode, d: var TLoc, op: TMagic) =
       typ.add "*"
 
     linefmt(p, cpsStmts, "$1 = ($2)($3);$n", [a.r, typ, rdLoc(b)])
-  of mChckIndex:
-    var arr, a: TLoc
-    initLocExpr(p, e[1], arr)
-    initLocExpr(p, e[2], a)
-    genIndexCheck(p, e[1], arr, a, e.exit)
   of mChckBounds:
     var arr, a, b: TLoc
     initLocExpr(p, e[1], arr)

compiler/mir/mirtypes.nim
@@ -41,6 +41,8 @@ type
     types: Store[TypeId, PType]
     sizeType: TypeId
       ## the target-dependent integer type to use for size values
+    usizeType: TypeId
+      ## the target-dependent unsigned integer type to use for size values
 
 const
   VoidType*    = TypeId 0
@@ -64,7 +66,8 @@ proc initTypeEnv*(graph: ModuleGraph): TypeEnv =
   ## Returns a fully initialized type environment instance.
   result = TypeEnv(map: default(TypeTable[TypeId]),
                    types: default(Store[TypeId, PType]),
-                   sizeType: VoidType)
+                   sizeType: VoidType,
+                   usizeType: VoidType)
 
   template add(kind: TTypeKind, expect: TypeId) =
     let
@@ -96,10 +99,10 @@ proc initTypeEnv*(graph: ModuleGraph): TypeEnv =
   add(tyInt,   TypeId(ord(PointerType) + 1))
   add(tyFloat, TypeId(ord(PointerType) + 2))
 
-  result.sizeType =
+  (result.sizeType, result.usizeType) =
     case graph.config.target.intSize
-    of 1, 2, 4: Int32Type
-    of 8:       Int64Type
+    of 1, 2, 4: (Int32Type, UInt32Type)
+    of 8:       (Int64Type, UInt64Type)
     else:       unreachable()
 
 proc add*(env: var TypeEnv, t: PType): TypeId =
@@ -116,3 +119,8 @@ func sizeType*(env: TypeEnv): TypeId {.inline.} =
   ## Returns the type to use for values representing some size. This is a
   ## signed integer type of target-dependent bit-width.
   env.sizeType
+
+func usizeType*(env: TypeEnv): TypeId {.inline.} =
+  ## Returns the type to use for values representing some size. This is an
+  ## unsigned integer type of target-dependent bit-width.
+  env.usizeType

compiler/sem/mirexec.nim
@@ -381,7 +381,7 @@ func computeDfg*(tree: MirTree): DataFlowGraph =
       # patch the join-to-instruction mapping:
       env.joins[id] = env.instrs.high.InstrPos
     else:
-      unreachable("unused join point")
+      discard "label is not used, ignore"
 
   template goto(pos: NodePosition, label: LabelId) =
     env.instrs.add Instr(op: opGoto, node: pos, dest: map(env, label))

commit 876540a95b907b8de6640763270c5ce5e3c76128

fix index check bugs affecting the C backend (#1327)
## Summary

Fix two bugs with index checks:
* accessing an `array` with a negative base index with a `uint` operand
  could result in false positive errors
* in some edge cases, array access would "wrap around", instead of a
  index error being reported; regression from
  https://github.com/nim-works/nimskull/pull/1324

Only the C backend was affected.

## Details

The MIR code produced by the `mChckIndex` lowering used different types
for the comparison operands, resulting in the C integer promotion rules
to apply, causing false positive and false negative results.

To fix the bug, the suggestion provided by the fixme is implemented,
namely that the boundary values are first converted to the index
operand's type prior to comparison.

If a boundary index value is not representable with the index operand's
type, the boundary test is omitted. Take, for example,
`array[-1..1, int]` that is index with a `uint` value: `uint` is
guaranteed to be >= 0, so the `-1 < idx` test can be folded away.

It's guaranteed that only one comparisons can be omitted, because
otherwise the array access can statically be proven to work, and no
index check must have been emitted by `mirgen` in the first place.

### Tests

* the `tout_of_range_index_check.nim` test is enabled for the C target
* a regression test for the fixed false negative check is added

compiler/mir/rtchecks.nim
@@ -219,40 +219,51 @@ proc emitIndexCheck(tree; call; graph; env; bu) =
 
   let
     idxOperand = NodePosition tree.argument(call, 1)
-    sizeType  = env.types.sizeType
-    usizeType = env.types.usizeType
+    sizeType   = env.types.sizeType
+    usizeType  = env.types.usizeType
+    conf       = graph.config
 
-  if ty.kind == tyArray and (firstOrd(graph.config, ty) != Zero or
-     lastOrd(graph.config, ty) < Zero):
-    # FIXME: this branch also need to be taken when lastOrd >= high(int)
-    # we need to test against both the lower and upper bound
+  if ty.kind == tyArray and (firstOrd(conf, ty) != Zero or
+     lengthOrd(conf, ty) > lastOrd(conf, env[sizeType])):
+    # we cannot check against just the length; the lower and upper bound need
+    # to be checked against separately
     let
-      typ   = env.types.add(env[tree[arrOperand].typ][0])
-      first = env.makeLiteral(mnkIntLit, firstOrd(graph.config, ty), typ)
-      last  = env.makeLiteral(mnkIntLit, lastOrd(graph.config, ty), typ)
+      idxTyp = env[tree[idxOperand].typ]
+      litKind = if isUnsigned(idxTyp): mnkUIntLit else: mnkIntLit
+      lo     = firstOrd(conf, ty)
+      hi     = lastOrd(conf, ty)
+      first  = env.makeLiteral(litKind, lo, tree[idxOperand].typ)
+      last   = env.makeLiteral(litKind, hi, tree[idxOperand].typ)
+      ltOp   = getMagicLessForType(idxTyp.skipTypes(abstractRange +
+                                                    tyUserTypeClasses)).lt
 
-    # FIXME: there are two problems here:
-    #        * the comparison operator is wrong for non-int types
-    #        * the comparison operands don't use the same type
-    #        As a consequence, C integer promotion rules apply, leading to
-    #        incorrect test results in some cases. The bound values need to
-    #        be converted to the index operand's type first, with boundary
-    #        checks omitted where the boundary's value cannot be represented
-    #        with the index operand's type
-
-    let cond = bu.wrapTemp BoolType:
-      bu.buildMagicCall mLtI, BoolType:
-        bu.subTree mnkArg:
-          bu.emitFrom(tree, idxOperand)
-        bu.emitByVal first
-
-    bu.buildIfNot cond:
-      bu.subTree mnkAsgn:
-        bu.use cond
-        bu.buildMagicCall mLtI, BoolType:
-          bu.emitByVal last
+    var cond: Value
+    if firstOrd(conf, idxTyp) < lo:
+      # lower bound needs to be checked against
+      cond = bu.wrapTemp BoolType:
+        bu.buildMagicCall ltOp, BoolType:
           bu.subTree mnkArg:
             bu.emitFrom(tree, idxOperand)
+          bu.emitByVal first
+
+    if lastOrd(conf, idxTyp) > hi:
+      # upper bound needs to be checked against
+      proc check(bu; tree; m: TMagic, src: NodePosition, val: Value) {.nimcall.} =
+        bu.buildMagicCall m, BoolType:
+          bu.emitByVal val
+          bu.subTree mnkArg:
+            bu.emitFrom(tree, src)
+
+      if firstOrd(conf, idxTyp) < lo:
+        # the upper bound only needs to be tested when the lower bound check
+        # succeeded
+        bu.buildIfNot cond:
+          bu.subTree mnkAsgn:
+            bu.use cond
+            check(bu, tree, ltOp, idxOperand, last)
+      else:
+        cond = bu.wrapTemp BoolType:
+          check(bu, tree, ltOp, idxOperand, last)
 
     bu.buildIf cond:
       bu.emitCall(tree, call, env.addCompilerProc(graph, "raiseIndexError3")):

tests/lang_types/array/tout_of_range_index_check.nim
@@ -3,10 +3,6 @@ discard """
     Ensure that accessing an array works when the index operand's type cannot
     be safely converted to the array's index type
   '''
-  knownIssue.c: '''
-    The boundary checks are implemented improperly, leading to the array
-    appearing to effectively be empty
-  '''
   knownIssue.vm: '''
     Arrays with a start index outside of -128..127 crash the code generator
   '''
@@ -27,3 +23,9 @@ proc test2(index: uint): int =
   result = arr[index]
 
 doAssert test2(1) == 3
+
+# regression test: make sure that an index that is valid after conversion to
+# uint gets caught by the index check
+when not defined(vm): # catching defects is not supported by the VM
+  doAssertRaises IndexDefect:
+    discard test1(low(int))