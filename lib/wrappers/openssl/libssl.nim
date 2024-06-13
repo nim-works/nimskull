34546a6797f57a9b4535884ff4485ad39d588038
Leorize <leorize+oss@disroot.org>
2024-06-13 16:20:05 -0500

    openssl: read OPENSSL_LIB_DIR env for library location

--- lib/wrappers/openssl/libssl.nim
+++ lib/wrappers/openssl/libssl.nim
@@ -1,4 +1,21 @@
-const nimLibsslLinkFlags {.strdefine.} = "-lssl"
+import os
+
+const nimOpensslLibDir {.strdefine.} = getEnv("OPENSSL_LIB_DIR")
+
+const nimLibsslName {.strdefine.} =
+  when defined(vcc):
+    "libssl"
+  else:
+    "ssl"
+
+proc generateLinkFlags(): string =
+  if nimOpensslLibDir.len > 0:
+    result.add quoteShell("-L" & nimOpensslLibDir)
+    result.add ' '
+
+  result.add quoteShell("-l" & nimLibsslName)
+
+const nimLibsslLinkFlags {.strdefine.} = generateLinkFlags()
 
 {.used.} # This module is only used for the side-effect
 {.passl: nimLibsslLinkFlags.}
