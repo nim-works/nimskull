6cfebd9bece9cb0c0a5f997936e8ab3e75008ed7
alaviss <leorize+oss@disroot.org>
2024-06-19 14:17:03 -0500

    Produce a meaningful error message for invalid module name (#1354)
    ## Summary
    While the AST form of the error generated by  `modules/modulepaths`  is
    handled in  `cli_reporter` , the symbol form was not, creating terrible
    error messages such as:
    
        Error: invalid module name '<nil tree>'
    
    This PR makes cli_reporter aware of the symbol form generated by 
    `modules/modules` , fixing the bad error message.
    
    ## Details
    *  `cli_reporter`  now handles the symbol form of 
    `rsemInvalidModuleName`  correctly.
    *  `modules/modules`  now raises  `rsemInvalidModuleName`  with line
    information, allowing user and tools to know which file the error
    originated from.
    
    Fixes https://github.com/nim-works/nimskull/issues/720
    
    ---------
    
    Co-authored-by: zerbina <100542850+zerbina@users.noreply.github.com>

--- compiler/front/cli_reporter.nim
+++ compiler/front/cli_reporter.nim
@@ -1636,7 +1636,10 @@ proc reportBody*(conf: ConfigRef, r: SemReport): string =
       result = "using '.' instead of '/' in import paths is deprecated"
 
     of rsemInvalidModuleName:
-      result = "invalid module name: '$1'" % r.ast.render
+      if r.sym != nil:
+        result = "invalid module name: '$1'" % r.symstr
+      else:
+        result = "invalid module name: '$1'" % r.ast.render
 
     of rsemInvalidMethodDeclarationOrder:
       result = "invalid declaration order; cannot attach '" & r.symbols[0].name.s &
