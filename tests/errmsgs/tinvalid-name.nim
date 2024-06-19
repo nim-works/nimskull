05757db7a988687b78cb3900bb8aab722f0fa47f
alaviss <leorize+oss@disroot.org>
2024-06-19 12:34:33 -0500

    tinvalid-name: styling
    Co-authored-by: zerbina <100542850+zerbina@users.noreply.github.com>

--- tests/errmsgs/tinvalid-name.nim
+++ tests/errmsgs/tinvalid-name.nim
@@ -1,4 +1,6 @@
 discard """
-  description: Test that a proper error message is printed for a module with invalid name
+  description: '''
+    Test that a proper error message is printed for a module with invalid name
+  '''
   errormsg: "invalid module name: 'tinvalid-name'"
 """
