105acc255e117bc85fb65af2617804b1920620ca
Adam Sherwood <adam.the.sherwood@gmail.com>
2024-07-01 11:50:53 +0100

    first pass with tests

--- compiler/sem/semmagic.nim
+++ compiler/sem/semmagic.nim
@@ -61,6 +61,22 @@ proc semSizeOf(c: PContext, n: PNode): PNode =
     result = c.config.newError(n, PAstDiag(kind: adSemMagicExpectTypeOrValue,
                                             magic: mSizeOf))
 
+proc semAlignOf(c: PContext, n: PNode): PNode =
+  case n.len
+  of 2:
+    n[1] = semExprWithType(c, n[1])
+    if containsGenericType(n[1].typ):
+      # report the type, not the typedesc
+      n[1] = c.config.newError(n[1], PAstDiag(kind: adSemTIsNotAConcreteType,
+                                              wrongType: n[1].typ[0]))
+      result = n
+    else:
+      n.typ = getSysType(c.graph, n.info, tyInt)
+      result = foldAlignOf(c.config, n, n)
+  else:
+    result = c.config.newError(n, PAstDiag(kind: adSemMagicExpectTypeOrValue,
+                                            magic: mAlignOf))
+
 type
   SemAsgnMode = enum asgnNormal, noOverloadedSubscript, noOverloadedAsgn
 
@@ -422,7 +438,7 @@ proc magicsAfterOverloadResolution(c: PContext, n: PNode,
   of mSizeOf:
     result = semSizeOf(c, n)
   of mAlignOf:
-    result = foldAlignOf(c.config, n, n)
+    result = semAlignOf(c, n) 
   of mOffsetOf:
     result = foldOffsetOf(c.config, n, n)
   of mArrGet:
