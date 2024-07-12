0ba6c1fb02008ba1e1aa27914e52ae473d8fa41f
zerbina <100542850+zerbina@users.noreply.github.com>
2024-07-12 01:27:11 +0000

    types: add `isPassByRef`
    The procedure is a backend-agnostic version of
    `ccgtypes.ccgIntroducedPtr`. `mirtypes` will need it for computing the
    parameter flags for procedural types.

--- compiler/ast/types.nim
+++ compiler/ast/types.nim
@@ -1557,3 +1557,32 @@ proc classifyBackendView*(t: PType): BackendViewKind =
      tyGenericParam, tyForward, tyBuiltInTypeClass, tyCompositeTypeClass,
      tyAnd, tyOr, tyNot, tyAnything, tyFromExpr:
     unreachable()
+
+proc isPassByRef*(conf: ConfigRef; s: PSym, retType: PType): bool =
+  var pt = skipTypes(s.typ, typedescInst)
+  assert skResult != s.kind
+
+  if tfByRef in pt.flags: return true
+  elif tfByCopy in pt.flags: return false
+  case pt.kind
+  of tyObject:
+    if s.typ.sym != nil and sfForward in s.typ.sym.flags:
+      # forwarded objects are *always* passed by pointers for consistency!
+      result = true
+    elif (optByRef in s.options) or (getSize(conf, pt) > conf.target.floatSize * 3):
+      result = true           # requested anyway
+    elif (tfFinal in pt.flags) and (pt[0] == nil):
+      result = false          # no need, because no subtyping possible
+    else:
+      result = true           # ordinary objects are always passed by reference,
+                              # otherwise casting doesn't work
+  of tyTuple:
+    result = (getSize(conf, pt) > conf.target.floatSize*3) or (optByRef in s.options)
+  else:
+    result = false
+
+  # first parameter and return type is 'lent T'? --> use pass by pointer if
+  # not already a pointer-like type
+  if s.position == 0 and retType != nil and retType.kind == tyLent:
+    result = pt.kind notin {tyVar, tyOpenArray, tyVarargs, tyRef, tyPtr,
+                            tyPointer}
