8a6fa584e194fc1c24a1b19863a9ff0ee35ccb5c
saem <saemghani+github@gmail.com>
2024-08-24 16:07:01 -0700

    existing import tests now properly use knownissue

--- tests/lang_stmts/importstmt/ttyped_import_nimskull757.nim
+++ tests/lang_stmts/importstmt/ttyped_import_nimskull757.nim
@@ -40,22 +40,3 @@ outputTyped:
   import mqux4 except bar4, baz4
   # import as output might have issues, see below
   import mqux5 as mqux6
-
-when false: # knownIssue: `import except` doesn't preserve exclusion info
-  # test should check for output:
-  #   ImportExceptStmt
-  #     Sym "mqux3"
-  #     Sym "bar3"
-  #   ImportExceptStmt
-  #     Sym "mqux4"
-  #     Sym "bar4"
-  #     Sym "baz4"
-  outputTyped:
-    import mqux3 except bar3
-    import mqux4 except bar4, baz4
-
-when false: # knownIssue: `Import as` doesn't preserve original identifier info
-  # not quite sure what the test should be yet, current behaviour might be
-  # acceptable, because the symbol decl can be introspected?
-  outputTyped:
-    import mqux5 as mqux6
