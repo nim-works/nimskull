c1ab4998dff689204c776a53de3162003ddb8f1e
saem <saemghani+github@gmail.com>
2024-08-24 16:17:32 -0700

    make the tests a bit more comprehensive

--- tests/lang_stmts/importstmt/timport_relative.nim
+++ tests/lang_stmts/importstmt/timport_relative.nim
@@ -2,6 +2,8 @@ discard """
   description: "Regression test for relative imports"
 """
 
+# relative to current directory
 import ./[mbar]
 import ./[mbaz, mfoo]
 import ./[mqux1 as mqux]
+import ./mqux2
