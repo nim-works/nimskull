061fc4d05febd7e9d5e28be52a8530062be3445b
saem <saemghani+github@gmail.com>
2024-09-03 09:48:29 -0700

    clean-up error message
    fix code that was clowning instead of actually using string formatting
    
    still not qutie right as there is _some_ flexibility with pragmas on
    definitions

--- tests/lang_callable/tpragma_redefinition_on_routine_def_err.nim
+++ tests/lang_callable/tpragma_redefinition_on_routine_def_err.nim
@@ -1,6 +1,6 @@
 discard """
   description: "Error when routine def has pragma not in prototype"
-  errormsg: "pragmas are only allowed in the header of a proc; redefinition of 'a' from"
+  errormsg: "pragmas are only allowed in the header of a routine; 'a'"
   line: 8
 """
 
