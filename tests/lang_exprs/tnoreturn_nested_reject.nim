d8631ad05460c9a892bde32b64856679c7697dad
saem <saemghani+github@gmail.com>
2024-09-07 13:33:32 -0700

    include test failure condition

--- tests/lang_exprs/tnoreturn_nested_reject.nim
+++ tests/lang_exprs/tnoreturn_nested_reject.nim
@@ -2,6 +2,8 @@ discard """
   description: '''
     Ensure nested non-noreturn statements aren't treated as such
   '''
+  errormsg: "expression '10' is of type 'int literal(10)' and has to be used (or discarded)"
+  line: 11
 """
 
 let x =
