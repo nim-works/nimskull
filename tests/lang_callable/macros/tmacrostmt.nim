6ccac53cd003a9b19b31a60761ac931ecb36a442
zerbina <100542850+zerbina@users.noreply.github.com>
2025-03-26 17:10:09 +0000

    tests: update `tmacrostmt.nim`
    The typed AST for routines with an expression body changed.

--- tests/lang_callable/macros/tmacrostmt.nim
+++ tests/lang_callable/macros/tmacrostmt.nim
@@ -116,9 +116,9 @@ proc fn6() =
 # bug #10807
 
 static:
-  let fn1s = "proc fn1(x, y: int): int =\n  result = 2 * (x + y)\n"
-  let fn2s = "proc fn2(x, y: float): float =\n  result = (y + 2.0 * x) / (x - y)\n"
-  let fn3s = "proc fn3(x, y: int): bool =\n  result = ((x and 3) div 4 or x mod (y xor -1)) == 0 or not contains([1, 2], y)\n"
+  let fn1s = "proc fn1(x, y: int): int =\n  return 2 * (x + y)\n"
+  let fn2s = "proc fn2(x, y: float): float =\n  return (y + 2.0 * x) / (x - y)\n"
+  let fn3s = "proc fn3(x, y: int): bool =\n  return ((x and 3) div 4 or x mod (y xor -1)) == 0 or not contains([1, 2], y)\n"
   let fn4s = "proc fn4(x: int): int =\n  if x mod 2 == 0:\n    return x + 2\n  else:\n    return 0\n"
   let fn5s = "proc fn5(a, b: float): float =\n  result = -a * a / (b * b)\n"
   let fn6s = "proc fn6() =\n  var a = @[1.0, 2.0]\n  let z = a{0, 1}\n  a{2} = 5.0\n"
