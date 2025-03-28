ffd200680fc3f337924c0abebdbecbc6e38eb2f1
zerbina <100542850+zerbina@users.noreply.github.com>
2025-03-28 17:53:35 +0100

    sem: wrap expression routine bodies in `return` (#1503)
    ## Summary
    
    Wrap expression routine bodies in `return` statements instead of
    `result` assignments. This preserves the information that the
    expression is returned directly.
    
    ## Details
    
    The information that the expression is returned directly gets lost when
    lowering it to a `result = body` statement, as those assignments cannot
    be distinguished from the ones written directly by the programmer.
    
    In addition, compared to the previous transform, the `return` is
    applied to the last expression in the list (if the body is an
    expression list), not the whole list.
    
    Both `docgen` and `suggest` previously had to look for doc comments and
    examples within trailing top-level assignment due to the whole body
    expression being used as the assignment RHS -- these workarounds are no
    longer necessary and thus removed.

--- compiler/sem/semexprs.nim
+++ compiler/sem/semexprs.nim
@@ -2369,11 +2369,17 @@ proc semProcBody(c: PContext, n: PNode): PNode =
       #   # comment
       # are not expressions:
       fixNilType(c, result)
+    elif result.kind == nkStmtListExpr:
+      # in order to preserve doc comments, apply the return to the last
+      # statement in the list, not to the whole list
+      result.transitionSonsKind(nkStmtList)
+      result.typ = nil
+      let last = semReturn(c, newTreeI(nkReturnStmt, n.info, result[^1]))
+      result[^1] = last
+      if last.kind == nkError:
+        result = c.config.wrapError(result)
     else:
-      var a = newNodeI(nkAsgn, n.info, 2)
-      a[0] = newSymNode(c.p.resultSym)
-      a[1] = result
-      result = semAsgn(c, a)
+      result = semReturn(c, newTreeI(nkReturnStmt, n.info, result))
   else:
     result = discardCheck(c, result, {})
 
