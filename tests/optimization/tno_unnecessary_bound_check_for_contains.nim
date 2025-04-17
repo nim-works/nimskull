a273a46664f737b5e2e50916b7bb6d7e9a28eb3b
zerbina <100542850+zerbina@users.noreply.github.com>
2025-04-17 18:18:44 +0000

    mirgen: fix internal type error
    Summary
    =======
    
    Fix a type error in the `mirgen`-produced MIR code. Observable program
    and compiler behaviour doesn't change.
    
    Details
    =======
    
    For `var x; x in {1, 2, 3}` where `x` has a type other than `int`,
    `mirgen` produced less-equal operations where the operands had a
    different types, which is disallowed according to the MIR's semantics.
    
    Nothing enforces these semantics, nor does anything rely on the
    operand's types being equal, so this went by unnoticed.
    
    To fix the issue, the upper or lower bound check is omitted if
    unnecessary, and the integer constant operand is changed to always use
    the same type as the to-be-range-checked value (`x` in the above
    example).

---
+++ tests/optimization/tno_unnecessary_bound_check_for_contains.nim
@@ -0,0 +1,18 @@
+discard """
+  action: compile
+  matrix: "--showir:mir_in:test --checks:off"
+  nimout: '''-- MIR: test
+scope:
+  def _2: bool
+  def _3: bool = leI(arg 0'i8, arg x)
+  if _3:
+    _2 := inSet(arg <D0>, arg x)
+  result = _2
+  goto [L1]
+L1:
+
+-- end'''
+"""
+
+proc test(x: int8): bool {.exportc.} =
+  x in {1, 2, 3}
