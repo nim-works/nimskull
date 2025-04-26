fb920984a449b72d45b78e227048412f8ca97a66
zerbina <100542850+zerbina@users.noreply.github.com>
2025-04-26 03:38:50 +0200

    varpartitions: fix memory leak with tuple unpacking (#1536)
    ## Summary
    
    Fix a compiler bug that led to destructors not being called for some
    local tuple-unpacking definitions.
    
    ## Details
    
    Locals introduced via tuple-unpacking `let`/`var` statements were
    allowed to be inferred as cursors. Since `mirgen` assumes that
    unpacked-into locals are always owning, values are moved into inferred-
    as-cursor unpack locals, causing memory leaks due to missing
    destruction.
    
    To fix the issue, locals introduced via tuple-unpacking are prevented
    from ever being cursors. Alternatively, `mirgen` could be changed to
    not assume unpacked-into locals always being owning, but this is more
    complex to implement.

---
+++ tests/arc/topt_unpacking_no_cursor.nim
@@ -0,0 +1,31 @@
+discard """
+  matrix: "--expandArc:test --hints:off"
+  nimout: '''--expandArc: test
+
+scope:
+  def x: (Obj, Obj) = init() -> [Resume]
+  def _2: (Obj, Obj) = move x
+  def a: Obj
+  a := move _2.0
+  def b: Obj
+  b := move _2.1
+  =destroy(name b)
+  =destroy(name a)
+
+-- end of expandArc ------------------------'''
+"""
+
+type Obj = object
+
+proc `=copy`(a: var Obj, b: Obj) =
+  discard
+
+proc init(): (Obj, Obj) =
+  discard
+
+proc test() =
+  let x = init()
+  let (a, b) = x
+  # ^^ the rhs is not something that *requires* taking ownership of
+
+test()
