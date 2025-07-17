f92755f25cdf62fcf2d57d0c2f0ba61b0396778a
zerbina <100542850+zerbina@users.noreply.github.com>
2025-07-17 19:23:38 +0000

    mirtress: fix `subNode`

--- compiler/mir/mirtrees.nim
+++ compiler/mir/mirtrees.nim
@@ -558,9 +558,9 @@ iterator pairs*(tree: MirTree): (NodePosition, lent MirNode) =
 iterator subNodes*(tree: MirTree, n: NodePosition; start = 0): NodePosition =
   ## Returns in order of apperance all direct child nodes of `n`, starting with
   ## `start`.
-  let L = tree[n].len
+  let L = int tree[n].len
   var n = tree.child(n, start)
-  for _ in 0..<L:
+  for _ in start..<L:
     yield n
     n = tree.sibling(n)
 
