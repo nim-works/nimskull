1b34e72c601ab052550bdf9869bdbd5f56105d57
zerbina <100542850+zerbina@users.noreply.github.com>
2025-07-29 01:21:23 +0000

    tailcall_analysis: remove dependency on `PContext`
    `genApply` is now usable when a `PContext` is not available. This is a
    preparation for creating .tailcall procedures after the sem phase.

--- compiler/sem/sempass2.nim
+++ compiler/sem/sempass2.nim
@@ -1946,7 +1946,7 @@ proc trackProc*(c: PContext; s: PSym, body: PNode) =
 
   if s.typ.callConv == ccTailcall:
     verifyTailCalls(g, s, body)
-    genApply(c, s)
+    genApply(c.graph, c.idgen, s)
 
     # create the type-bound ops for the continuation type:
     let cont = s.typ.n[0][3].typ.skipTypes(skipForHooks)
