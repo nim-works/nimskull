d6dddf76ed9c04a922183108e5328b14dd3743a6
Leorize <leorize+oss@disroot.org>
2025-08-13 20:53:10 -0500

    macros: remove default `ident` parameter for old gensym
    Otherwise this causes `genSym()` to be ambiguous. The older variant is
    still required for bootstrap and is moved to another branch.

--- lib/core/macros.nim
+++ lib/core/macros.nim
@@ -438,13 +438,17 @@ when defined(nimskullHasUnaryGenSym):
     ## Generates a fresh symbol that is guaranteed to be unique. The symbol
     ## needs to occur in a declaration context.
 
-proc genSym*(kind: NimSymKind = nskLet; ident = ""): NimNode {.
-  magic: "NGenSym", noSideEffect, deprecated: "genSym no longer takes a `kind` parameter".}
-  ## Generates a fresh symbol that is guaranteed to be unique. The symbol
-  ## needs to occur in a declaration context.
-  ##
-  ## This is a compatibility alias for `genSym`, the `kind` parameter is
-  ## ignored.
+  proc genSym*(kind: NimSymKind = nskGenerated; ident: string): NimNode {.
+    magic: "NGenSym", noSideEffect, deprecated: "genSym no longer takes a `kind` parameter".}
+    ## Generates a fresh symbol that is guaranteed to be unique. The symbol
+    ## needs to occur in a declaration context.
+    ##
+    ## This is a compatibility alias for `genSym`, the `kind` parameter is
+    ## ignored.
+else:
+  # Old prototype for bootstrapping
+  proc genSym*(kind: NimSymKind = nskLet; ident = ""): NimNode {.
+    magic: "NGenSym", noSideEffect.}
 
 proc callsite*(): NimNode {.magic: "NCallSite", benign, deprecated:
   "Deprecated since v0.18.1; use `varargs[untyped]` in the macro prototype instead".}
