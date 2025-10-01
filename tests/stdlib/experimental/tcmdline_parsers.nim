66a16f53882e64b545d2057baede809779f69f27
Luyten-Orion <raeeshussain960@gmail.com>
2025-10-01 18:50:21 +0100

    Undo cmdline test merge.

--- tests/stdlib/experimental/tcmdline_parsers.nim
+++ tests/stdlib/experimental/tcmdline_parsers.nim
@@ -17,13 +17,6 @@ block intParser:
   doAssertRaises(ValueError):
     discard parseCli(uint8, "-1")
 
-block floatParser:
-  doAssert parseCli(float64, "3.14") == 3.14
-  doAssert parseCli(float64, "-3.14") == -3.14
-  doAssert parseCli(float32, "nan").isNaN
-  doAssert parseCli(float32, "inf") == Inf
-  doAssert parseCli(float32, "-inf") == -Inf
-
 block stringParser:
   doAssert parseCli(string, "foo bar") == "foo bar"
 
