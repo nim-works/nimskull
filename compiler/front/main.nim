fe9115925434708198d9e343fbb909d13124c555
zerbina <100542850+zerbina@users.noreply.github.com>
2025-10-01 23:56:45 +0000

    main: reduce peak memory usage by a little
    The VM instance doesn't need to stay around during the mid-end and code
    generation phases.

--- compiler/front/main.nim
+++ compiler/front/main.nim
@@ -200,6 +200,8 @@ proc commandCompileToC(graph: ModuleGraph) =
 
   graph.config.timeTracer.traceStr("compile"):
     compileProject(graph)
+  # semantic analysis is done, dismiss the VM instance to save memory
+  graph.vm = nil
   prepareForCodegen(graph)
   if conf.symbolFiles == disabledSf:
     cbackend2.generateCode(graph, graph.takeModuleList())
