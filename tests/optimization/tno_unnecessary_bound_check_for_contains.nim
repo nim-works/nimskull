8a91190287f1ab233fe088794114a3230353ebab
zerbina <100542850+zerbina@users.noreply.github.com>
2025-10-05 20:00:53 +0200

    utils: make MIR pretty-printing output more stable (#1632)
    ## Summary
    
    Pretty-print the anonymous constants as their value in `--expandArc`
    and `--showir` output.
    
    ## Details
    
    When the MIR environment is available, anonymous constants are now
    rendered as their value, not as their raw ID (which is dependent on
    externalities unrelated to the body of the procedure).
    
    The goal is to keep tests inspecting `--expandArc` and `--showir`
    output from starting to fail when unrelated processing changes in
    the future -- the tests affected by the change in rendering are
    updated.

--- tests/optimization/tno_unnecessary_bound_check_for_contains.nim
+++ tests/optimization/tno_unnecessary_bound_check_for_contains.nim
@@ -6,7 +6,7 @@ scope:
   def _2: bool
   def _3: bool = leI(arg 0'i8, arg x)
   if _3:
-    _2 := inSet(arg <D0>, arg x)
+    _2 := inSet(arg <const> {1, 2, 3}, arg x)
   result = _2
   goto [L1]
 L1:
