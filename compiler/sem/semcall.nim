3197f19f10b5cb84161e27184b5632d876308e66
saem <saemghani+github@gmail.com>
2025-10-13 12:13:39 -0700

    remove error node wrapping in `semResolvedCall`
    Wrapping nodes is no longer necesssary as they're reported immediately.
    This was resulting in attempting to wrap nodes that no longer contain
    errors but resulted in them.

--- compiler/sem/semcall.nim
+++ compiler/sem/semcall.nim
@@ -548,7 +548,7 @@ proc semResolvedCall(c: PContext, x: TCandidate,
     result[0] = newSymNode(finalCallee, getCallLineInfo(result[0]))
     if x.fauxMatch == tyError:
       # at least one argument expression was erroneous
-      result = c.config.wrapError(result)
+      discard "already reported"
     elif containsGenericType(result.typ) or x.fauxMatch == tyUnknown:
       result.typ = newTypeS(x.fauxMatch, c)
       if result.typ.kind == tyError: incl result.typ.flags, tfCheckedForDestructor
