07cd6440182b03919fa2b2f23144f21eaacee678
Leorize <leorize+oss@disroot.org>
2025-11-29 23:00:24 +0700

    mir2cg: handle casts between imported types
    When the size cannot be computed at compile-time, offload the
    computation to the target instead of passing through special size
    values.

---
+++ tests/lang/s05_pragmas/s01_interop/t08_imported_struct_cast.nim
@@ -0,0 +1,30 @@
+discard """
+  description: '''
+    Imported C struct types can be casted to other types.
+  '''
+  targets: "c"
+  joinable: false
+"""
+
+type
+  ExternBig {.importc: "struct Struct1", header: "t08_imported_struct_cast.nim.h".} = object
+    field1: cint
+    field2: cint
+    # not all fields need to exposed
+
+  ExternSmall {.importc: "struct Struct2", header: "t08_imported_struct_cast.nim.h"} = object
+    field1: cint
+
+  Intern = object
+    field1: cint
+
+let v1 = ExternBig(field1: 1, field2: 2)
+let v2 = cast[ExternSmall](v1)
+let v3 = cast[Intern](v1)
+
+doAssert v1.field1 == 1
+doAssert v1.field2 == 2
+
+doAssert v2.field1 == 1
+
+doAssert v3.field1 == 1
