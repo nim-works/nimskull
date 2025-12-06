84cd84c4ba9b74888f044211823c3665221f9c67
zerbina <100542850+zerbina@users.noreply.github.com>
2025-12-06 18:55:38 +0000

    sempass2: always initialize the effect list
    This gets rid of the "missing effect list" state, at the cost of slightly
    increased memory usage.

--- compiler/sem/sempass2.nim
+++ compiler/sem/sempass2.nim
@@ -1514,26 +1514,39 @@ proc checkMethodEffects*(g: ModuleGraph; disp, branch: PSym) =
           [$branch.typ.lockLevel, $disp.typ.lockLevel])
 
 proc setEffectsForProcType*(g: ModuleGraph; t: PType, n: PNode; s: PSym = nil) =
+  ## Initializes the effect lists for the proc type `t`. `n` is the pragma
+  ## list to take the explicit effect specification (if any) from, while `s`
+  ## is the symbol of the routine the type is attached to.
+  internalAssert(g.config, t.kind == tyProc)
   var effects = t.n[0]
-  if t.kind != tyProc or effects.kind != nkEffectList: return
-  if n.kind != nkEmpty:
-    internalAssert(g.config, isNoEffectList(effects), "Starting effects list must be empty")
+  internalAssert(g.config, effects.kind == nkEffectList)
+  internalAssert(g.config, effects.len == 0, "Starting effects list must be empty")
+  newSeq(effects.sons, effectListLen)
 
-    if effects.len < effectListLen:
-      newSeq(effects.sons, effectListLen)
-    let raisesSpec = effectSpec(n, wRaises)
+  let raisesSpec = if n.kind == nkPragma: effectSpec(n, wRaises) else: nil
+  let exc =
     if not isNil(raisesSpec):
-      effects[exceptionEffects] = raisesSpec
+      raisesSpec
     elif s != nil and (s.magic != mNone or {sfImportc, sfExportc} * s.flags == {sfImportc}):
-      effects[exceptionEffects] = newNodeI(nkArgList, effects.info)
+      # cannot have exception effects by itself
+      newNodeI(nkBracket, effects.info)
+    else:
+      nil # means both "not computed yet" and "any exceptions"
 
-    let tagsSpec = effectSpec(n, wTags)
+  let tagsSpec = if n.kind == nkPragma: effectSpec(n, wTags) else: nil
+  let tags =
     if not isNil(tagsSpec):
-      effects[tagEffects] = tagsSpec
+      tagsSpec
     elif s != nil and (s.magic != mNone or {sfImportc, sfExportc} * s.flags == {sfImportc}):
-      effects[tagEffects] = newNodeI(nkArgList, effects.info)
+      # cannot have tag effects by itself
+      newNodeI(nkBracket, effects.info)
+    else:
+      nil # means both "not computed yet" and "any tags"
 
-    effects[pragmasEffects] = n
+  effects[exceptionEffects] = exc
+  effects[tagEffects] = tags
+  effects[pragmasEffects] = n
+
   if s != nil and s.magic != mNone:
     if s.magic != mEcho:
       t.flags.incl tfNoSideEffect
